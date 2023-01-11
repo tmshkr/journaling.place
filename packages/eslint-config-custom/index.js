bbbfe66193c6dc8121094d1a7fc8d5cc34abac13
Tim Shaker <tim@tmshkr.com>
2023-01-11 13:43:39 -0800

    dockerize

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -3,5 +3,8 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "turbo/no-undeclared-env-vars": "off",
+    "@next/next/no-img-element": "off",
+    "react-hooks/exhaustive-deps": "off",
   },
 };
