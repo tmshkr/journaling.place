4ced722f737f0ecbfedcf1bda68b81df75958cf6
Tim Shaker <tim@tmshkr.com>
2023-01-12 10:27:47 -0800

    add ENVIRONMENT to .env

--- .platform/hooks/predeploy/get-env-vars.sh
+++ .platform/hooks/predeploy/get-env-vars.sh
@@ -7,3 +7,6 @@ ENVIRONMENT=$(cat ENVIRONMENT)
 # get staging or production environment variables
 CONFIG_S3_BUCKET=$(/opt/elasticbeanstalk/bin/get-config environment -k CONFIG_S3_BUCKET)
 aws s3 cp s3://$CONFIG_S3_BUCKET/$ENVIRONMENT.env .env
+echo "Environment variables loaded from S3"
+
+printf "\nENVIRONMENT=$ENVIRONMENT\n" >> .env
