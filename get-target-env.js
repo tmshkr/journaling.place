f49be7a860ab6635db067d70dcb91b7fb61f4ef2
Tim Shaker <tim@tmshkr.com>
2023-01-15 19:26:25 -0800

    use execSync

--- get-target-env.js
+++ get-target-env.js
@@ -1,126 +1,83 @@
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 
 const { APP_NAME, BLUE_ENV, GREEN_ENV, PRODUCTION_CNAME, STAGING_CNAME } =
   process.env;
 
-async function getTargetEnv() {
-  return new Promise((resolve, reject) => {
-    exec(
-      `aws elasticbeanstalk describe-environments --application-name ${APP_NAME}`,
-      async (error, stdout, stderr) => {
-        if (error) {
-          console.error(`error: ${error.message}`);
-          return reject();
-        }
-        if (stderr) {
-          console.error(`stderr: ${stderr}`);
-          return reject();
-        }
+function getTargetEnv() {
+  const stdout = execSync(
+    `aws elasticbeanstalk describe-environments --application-name ${APP_NAME}`
+  );
 
-        const { Environments } = JSON.parse(stdout);
-        const targetEnv = Environments.find(
-          ({ CNAME, Status }) =>
-            CNAME.startsWith(STAGING_CNAME) && Status !== "Terminated"
-        );
+  const { Environments } = JSON.parse(stdout);
+  const targetEnv = Environments.find(
+    ({ CNAME, Status }) =>
+      CNAME.startsWith(STAGING_CNAME) && Status !== "Terminated"
+  );
 
-        if (!targetEnv) {
-          console.error("Target environment does not exist.");
-          const prodEnv = Environments.find(
-            ({ CNAME, Status }) =>
-              CNAME.startsWith(PRODUCTION_CNAME) && Status !== "Terminated"
-          );
-          const newEnv =
-            prodEnv?.EnvironmentName === GREEN_ENV ? BLUE_ENV : GREEN_ENV;
-          await createEnvironment(newEnv);
-          return resolve(await getTargetEnv());
-        }
-
-        if (targetEnv.Status !== "Ready") {
-          console.error("Target environment is not ready.");
-          return reject();
-        }
-
-        if (targetEnv.Health !== "Green") {
-          console.log("Target environment is not healthy.");
-          await terminateEnvironment(targetEnv.EnvironmentId);
-          await createEnvironment(targetEnv.EnvironmentName);
-          return resolve(await getTargetEnv());
-        }
-
-        resolve(targetEnv);
-      }
+  if (!targetEnv) {
+    console.error("Target environment does not exist.");
+    const prodEnv = Environments.find(
+      ({ CNAME, Status }) =>
+        CNAME.startsWith(PRODUCTION_CNAME) && Status !== "Terminated"
     );
-  });
+    const newEnv =
+      prodEnv?.EnvironmentName === GREEN_ENV ? BLUE_ENV : GREEN_ENV;
+    createEnvironment(newEnv);
+    return getTargetEnv();
+  }
+
+  if (targetEnv.Status !== "Ready") {
+    throw new Error("Target environment is not ready.");
+  }
+
+  if (targetEnv.Health !== "Green") {
+    console.log("Target environment is not healthy.");
+    terminateEnvironment(targetEnv.EnvironmentId);
+    createEnvironment(targetEnv.EnvironmentName);
+    return getTargetEnv();
+  }
+
+  return targetEnv;
 }
 
-async function createEnvironment(envName) {
-  console.log("Creating new environment... ", envName);
-  return new Promise((resolve, reject) => {
-    exec(
-      `aws elasticbeanstalk create-environment --application-name ${APP_NAME} --environment-name ${envName} --cname-prefix ${STAGING_CNAME} --template-name single-instance`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`error: ${error.message}`);
-          return reject();
-        }
-        if (stderr) {
-          console.error(`stderr: ${stderr}`);
-          return reject();
-        }
-        const newEnv = JSON.parse(stdout);
-        console.log("Creating env:", newEnv);
-        exec(
-          `aws elasticbeanstalk wait environment-exists --environment-ids ${newEnv.EnvironmentId}`,
-          (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log("Created environment");
-            resolve(newEnv);
-          }
-        );
-      }
-    );
-  });
+function createEnvironment(envName) {
+  console.log("Creating new environment... ");
+  const stdout = execSync(
+    `aws elasticbeanstalk create-environment --application-name ${APP_NAME} --environment-name ${envName} --cname-prefix ${STAGING_CNAME} --template-name single-instance`
+  );
+
+  const newEnv = JSON.parse(stdout);
+  console.log("Creating env:", newEnv);
+  execSync(
+    `aws elasticbeanstalk wait environment-exists --environment-ids ${newEnv.EnvironmentId}`
+  );
+  console.log("Created environment");
 }
 
-async function terminateEnvironment(environmentId) {
+function terminateEnvironment(environmentId) {
   console.log("Terminating environment... ", environmentId);
-  return new Promise((resolve, reject) => {
-    exec(
-      `aws elasticbeanstalk terminate-environment --environment-id ${environmentId}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`error: ${error.message}`);
-          return reject();
-        }
-        if (stderr) {
-          console.error(`stderr: ${stderr}`);
-          return reject();
-        }
-        console.log(stdout);
-        exec(
-          `aws elasticbeanstalk wait environment-terminated --environment-ids ${environmentId}`,
-          (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log("Terminated environment");
-            resolve();
-          }
-        );
-      }
-    );
-  });
+  execSync(
+    `aws elasticbeanstalk terminate-environment --environment-id ${environmentId}`
+  );
+  execSync(
+    `aws elasticbeanstalk wait environment-terminated --environment-ids ${environmentId}`
+  );
+  console.log("Terminated environment");
 }
 
 async function main() {
   let tries = 0;
   while (tries < 6) {
     try {
-      const targetEnv = await getTargetEnv();
-      if (!targetEnv) return;
+      const targetEnv = getTargetEnv();
       console.log("targetEnv", targetEnv);
-      exec(`echo TARGET_ENV=${targetEnv.EnvironmentName} >> $GITHUB_OUTPUT`);
+      execSync(
+        `echo TARGET_ENV=${targetEnv.EnvironmentName} >> $GITHUB_OUTPUT`
+      );
       break;
     } catch (error) {
       tries++;
+      console.error(error);
       console.log("Retrying...");
       await new Promise((resolve) => setTimeout(resolve, 1000 * 2 ** tries));
     }
