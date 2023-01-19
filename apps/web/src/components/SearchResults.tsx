5921fa8a282d52a5bb89280377005b208ade8498
Tim Shaker <tim@tmshkr.com>
2023-01-18 18:05:56 -0800

    render promptText

--- apps/web/src/components/SearchResults.tsx
+++ apps/web/src/components/SearchResults.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useQueryClient } from "react-query";
+
 export function SearchResults({
   search,
   setSearch,
@@ -8,13 +10,20 @@ export function SearchResults({
   if (searchResults.length === 0) {
     return null;
   }
+  const queryClient = useQueryClient();
+  const cache = queryClient.getQueryData("journal");
+
+  if (!cache) {
+    return null;
+  }
+
   return (
     <div className="absolute w-full bg-white border border-gray-200 rounded-md shadow-lg">
-      {searchResults.map((result) => {
-        const promptId = result.split("_")[1];
+      {searchResults.map((key) => {
+        const promptId = key.split("_")[1];
         return (
           <Link
-            key={result}
+            key={key}
             href={`/${promptId}`}
             onClick={() => {
               setSearch("");
@@ -22,7 +31,7 @@ export function SearchResults({
             }}
             className="block font-medium text-gray-900"
           >
-            {result}
+            {cache[key].promptText}
           </Link>
         );
       })}
