8b519a2bb8114ac9a7e5415364872e2fd80dae07
Tim Shaker <tim@tmshkr.com>
2023-01-20 17:16:38 -0800

    add update-dynamic-dns.js

---
+++ update-dynamic-dns.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const { execSync } = require("child_process");
+
+const { APP_NAME, DNS_USERNAME, DNS_PASSWORD, PRODUCTION_CNAME } = process.env;
+
+async function updateDNS() {
+  const stdout = execSync(
+    `aws elasticbeanstalk describe-environments --application-name ${APP_NAME}`
+  );
+
+  const { Environments } = JSON.parse(stdout);
+  const targetEnv = Environments.find(
+    ({ CNAME, Status }) =>
+      CNAME.startsWith(PRODUCTION_CNAME) && Status !== "Terminated"
+  );
+
+  await axios
+    .post(
+      `https://${DNS_USERNAME}:${DNS_PASSWORD}@domains.google.com/nic/update?hostname=journaling.place&myip=${targetEnv.EndpointURL}`
+    )
+    .then(({ data }) => console.log(`DNS response: ${data}`));
+}
+
+async function main() {
+  let tries = 0;
+  while (tries < 6) {
+    try {
+      await updateDNS();
+      break;
+    } catch (error) {
+      const seconds = 2 ** tries;
+      tries++;
+      console.error(error);
+      console.log(`Retrying in ${seconds}s...`);
+      await new Promise((resolve) => setTimeout(resolve, 1000 * seconds));
+    }
+  }
+}
+
+main();
