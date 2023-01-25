04cb2c589fee3f48dfd389ad71079b35b5bd5bfb
Tim Shaker <tim@tmshkr.com>
2023-01-24 17:52:57 -0800

    use ecr

--- .platform/hooks/predeploy/get-env-vars.sh
+++ .platform/hooks/predeploy/get-env-vars.sh
@@ -3,6 +3,7 @@ set -e
 
 # get environment
 ENVIRONMENT=$(cat ENVIRONMENT)
+SHA=$(cat SHA)
 
 # get staging or production environment variables
 CONFIG_S3_BUCKET=$(/opt/elasticbeanstalk/bin/get-config environment -k CONFIG_S3_BUCKET)
@@ -10,3 +11,4 @@ aws s3 cp s3://$CONFIG_S3_BUCKET/$ENVIRONMENT.env .env
 echo "Environment variables loaded from S3"
 
 printf "\nENVIRONMENT=$ENVIRONMENT\n" >> .env
+printf "\nSHA=$SHA\n" >> .env
