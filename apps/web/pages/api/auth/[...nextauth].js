28267e4aef2ba34bc49e73a818cdd28d85608e48
Tim Shaker <tim@tmshkr.com>
2023-01-26 15:34:12 -0800

    add link

--- apps/web/pages/api/auth/[...nextauth].js
+++ apps/web/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { prisma } from "src/lib/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import EmailProvider from "next-auth/providers/email";
+import { sendVerificationRequest } from "src/auth/sendVerificationRequest";
 
 BigInt.prototype.toJSON = function () {
   return this.toString();
@@ -15,6 +16,7 @@ export const authOptions = {
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
     }),
   ],
   session: {
