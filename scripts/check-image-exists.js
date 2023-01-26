1708f3df102497237b517a81811fc6139eb65769
Tim Shaker <tim@tmshkr.com>
2023-01-25 21:12:31 -0800

    refactor

--- scripts/check-image-exists.js
+++ scripts/check-image-exists.js
@@ -1,16 +1,19 @@
-const { execSync } = require("child_process");
-try {
-  execSync(
-    `aws ecr describe-images --repository-name=journaling.place --image-ids imageTag=${process.argv[2]}`
-  );
-  console.log("Image exists.");
-  execSync(`echo IMAGE_EXISTS=true >> $GITHUB_OUTPUT`);
-} catch (error) {
-  if (error.message.includes("ImageNotFoundException")) {
-    console.log("Image does not exist.");
-    execSync(`echo IMAGE_EXISTS=false >> $GITHUB_OUTPUT`);
-  } else {
-    console.error(error);
-    process.exit(1);
+const { exec, execSync } = require("child_process");
+
+exec(
+  `aws ecr describe-images --repository-name=journaling.place --image-ids imageTag=${process.argv[2]}`,
+  (error, stdout, stderr) => {
+    if (error) {
+      if (error.message.includes("ImageNotFoundException")) {
+        console.log("Image does not exist.");
+        execSync(`echo IMAGE_EXISTS=false >> $GITHUB_OUTPUT`);
+      } else {
+        console.error(error);
+        process.exit(1);
+      }
+    } else {
+      console.log("Image exists.");
+      execSync(`echo IMAGE_EXISTS=true >> $GITHUB_OUTPUT`);
+    }
   }
-}
+);
