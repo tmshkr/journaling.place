2cfd116c081cc4068270a08e787718c2a77ed52a
Tim Shaker <tim@tmshkr.com>
2023-01-27 15:24:12 -0800

    config

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -5,7 +5,7 @@ import { devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require("dotenv").config({ path: "../../.env" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -32,14 +32,17 @@ const config: PlaywrightTestConfig = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  globalSetup: "./global-setup",
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.NEXTAUTH_URL,
+    storageState: "storageState.json",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    video: "on",
   },
 
   /* Configure projects for major browsers */
@@ -96,12 +99,6 @@ const config: PlaywrightTestConfig = {
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 };
 
 export default config;
