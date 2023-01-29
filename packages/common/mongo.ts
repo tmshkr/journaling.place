180c309f18022bfd435ee0bfe4c12555dbc62671
Tim Shaker <tim@tmshkr.com>
2023-01-29 13:00:02 -0800

    tsconfig

--- packages/common/mongo.ts
+++ packages/common/mongo.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-export const mongoClient = new MongoClient(process.env.MONGO_URI);
+export const mongoClient = new MongoClient(process.env.MONGO_URI as string);
 mongoClient.connect().then(() => {
   console.log("Connected to MongoDB");
 });
