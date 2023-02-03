75257118ebf8eb24fe78864ffc5071cc0783f0c1
Tim Shaker <tim@tmshkr.com>
2023-02-02 17:06:55 -0800

    update playwright config

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -5,7 +5,7 @@ import { devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require("dotenv").config({ path: "../../.env" });
+require("dotenv").config({ path: "../../.env" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
