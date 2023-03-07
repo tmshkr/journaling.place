44df6e2db4ee7e53716e374e486814adcd48536a
Tim Shaker <tim@tmshkr.com>
2023-03-06 18:39:26 -0800

    cd SCRIPT_DIR

--- packages/pg-backup/run.sh
+++ packages/pg-backup/run.sh
@@ -2,7 +2,9 @@
 set -e
 
 ts=$(date +"%Y-%m-%dT%H:%M:%S%z")
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+cd $SCRIPT_DIR
 aws s3 cp s3://journaling.place-cfg/production.env .env
 docker compose up
 zip $ts.zip dump.sql
