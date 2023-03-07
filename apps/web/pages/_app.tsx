e6fcce224c961e021d10abe4a8c8d3bdf00b5433
Tim Shaker <tim@tmshkr.com>
2023-03-07 14:58:04 -0800

    remove pwa meta tags

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -48,17 +48,6 @@ export default function App({
           href="https://fonts.googleapis.com/css2?family=Neuton&display=swap"
           rel="stylesheet"
         />
-        <meta name="application-name" content="PWA App" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
       </Head>
       <DefaultSeo {...SEO} />
       <SessionProvider session={session}>
