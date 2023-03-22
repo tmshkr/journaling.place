71e2bb4d6bf428c85a031195f85f9373c67a9c60
Tim Shaker <tim@tmshkr.com>
2023-03-22 14:55:15 -0700

    DiceIcon

--- apps/web/src/components/modals/CreateNewEntryModal.tsx
+++ apps/web/src/components/modals/CreateNewEntryModal.tsx
@@ -7,6 +7,7 @@ import {
   DocumentTextIcon,
   DocumentIcon,
 } from "@heroicons/react/20/solid";
+import DiceIcon from "@fortawesome/fontawesome-free/svgs/solid/dice.svg";
 
 export function CreateNewEntryModal() {
   const dispatch = useAppDispatch();
@@ -27,17 +28,14 @@ export function CreateNewEntryModal() {
           type="button"
           className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          <DocumentDuplicateIcon
-            className="-ml-0.5 h-5 w-5"
-            aria-hidden="true"
-          />
+          <DocumentTextIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
           This Prompt
         </button>
         <button
           type="button"
           className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          <DocumentTextIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
+          <DiceIcon className="fill-white -ml-0.5 h-5 w-5" aria-hidden="true" />
           Random Prompt
         </button>
         <button
