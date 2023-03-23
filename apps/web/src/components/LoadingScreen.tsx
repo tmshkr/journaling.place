e2df7d5e1a9d3f8f524171adb0dfd4cb64eaedf2
Tim Shaker <tim@tmshkr.com>
2023-03-22 17:31:19 -0700

    adjust padding

--- apps/web/src/components/LoadingScreen.tsx
+++ apps/web/src/components/LoadingScreen.tsx
@@ -5,7 +5,7 @@ export function LoadingScreen() {
   const loading = useAppSelector(selectLoadingState);
   if (Object.keys(loading).some((item) => loading[item])) {
     return (
-      <div className="fixed top-0 right-0 bottom-0 left-0 z-50 bg-white flex items-center justify-center h-screen">
+      <div className="fixed top-0 right-0 bottom-0 left-0 z-50 bg-white pt-[21vh]">
         <div className="flex flex-col items-center">
           <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-indigo-500"></div>
           <h1 className="mt-4 text-2xl">Loading...</h1>
