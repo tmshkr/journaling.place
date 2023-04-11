189e324aa3e4618a149b36f857a7b6058c7ae79f
Tim Shaker <tim@tmshkr.com>
2023-04-11 14:32:24 -0700

    use quillWorker

--- apps/web/src/store/journal.ts
+++ apps/web/src/store/journal.ts
@@ -11,17 +11,31 @@ export const journalIndex = new Index({
 });
 
 export async function getJournals(cache = {}, cursor = undefined) {
+  if (typeof window === "undefined") return;
+  const Quill = await import("quill").then((value) => value.default);
+  const quillWorker = new Quill(document.createElement("div"));
+
   const nextCursor = await axios
     .get("/api/journal", { params: { cursor } })
     .then(async ({ data }) => {
       const { journals, nextCursor } = data;
       for (const entry of journals) {
         cache[entry.id] = entry;
+        journalIndex.add(Number(entry.id), entry.promptText);
+
         entry.ciphertext = toArrayBuffer(entry.ciphertext.data);
         entry.iv = new Uint8Array(entry.iv.data);
         entry.decrypted = await decrypt(entry.ciphertext, entry.iv);
-        journalIndex.add(Number(entry.id), entry.promptText);
-        journalIndex.append(Number(entry.id), entry.decrypted);
+
+        try {
+          quillWorker.setContents(JSON.parse(entry.decrypted));
+          entry.plaintext = quillWorker.getText();
+          journalIndex.append(Number(entry.id), entry.plaintext);
+        } catch (err) {
+          entry.plaintext = entry.decrypted;
+        }
+
+        journalIndex.append(Number(entry.id), entry.plaintext);
       }
       return nextCursor;
     });
