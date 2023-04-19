6b212e22b80659a6caabd408c4f82ead3545bf50
Tim Shaker <tim@tmshkr.com>
2023-04-19 16:45:39 -0700

    update schema

---
+++ prisma/migrations/20230419232943_journal_status/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "JournalStatus" AS ENUM ('ACTIVE', 'TRASHED', 'DELETED');
+
+-- AlterTable
+ALTER TABLE "Journal" ADD COLUMN     "status" "JournalStatus" NOT NULL DEFAULT 'ACTIVE',
+ALTER COLUMN "ciphertext" DROP NOT NULL,
+ALTER COLUMN "iv" DROP NOT NULL;
+
+CREATE FUNCTION prevent_update_on_deleted_record()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF OLD.status = 'DELETED' THEN
+    RAISE EXCEPTION 'Cannot update a deleted record';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER deleted_constraint_trigger
+BEFORE UPDATE ON "Journal"
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_on_deleted_record();
