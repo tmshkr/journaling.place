c06bc08cce9550170951f89e1c6620a131bf6635
Tim Shaker <tim@tmshkr.com>
2023-04-19 10:35:03 -0700

    update trash route

--- apps/web/src/pages/api/journal/[id]/trash.ts
+++ apps/web/src/pages/api/journal/[id]/trash.ts
@@ -2,11 +2,22 @@ import { withUser } from "src/middleware/withUser";
 import { prisma } from "src/lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
+import { z } from "zod";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
-router.use(withUser).patch(handlePatch);
+router.use(withUser).patch(handlePatch).delete(handleDelete);
 
-async function handlePatch(req, res) {
+export async function handlePatch(req, res) {
+  try {
+    z.object({
+      status: z.enum(["TRASHED", "ACTIVE"]),
+    }).parse(req.body);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ errorMessage: "Bad Request" });
+  }
+
+  const { status } = req.body;
   await prisma.journal
     .update({
       where: {
@@ -16,7 +27,30 @@ async function handlePatch(req, res) {
         },
       },
       data: {
-        status: "TRASHED",
+        status,
+      },
+    })
+    .catch((err) => {
+      if (err.code === "P2025") {
+        return res.status(404).json({ errorMessage: "Not Found" });
+      }
+      throw err;
+    });
+
+  return res.send();
+}
+
+async function handleDelete(req, res) {
+  await prisma.journal
+    .update({
+      where: {
+        id_authorId: {
+          id: BigInt(req.query.id),
+          authorId: BigInt(req.user.id),
+        },
+      },
+      data: {
+        status: "DELETED",
       },
     })
     .catch((err) => {
