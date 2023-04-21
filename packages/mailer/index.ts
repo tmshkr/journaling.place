db8f39387da784edc7eadd1906bf91c11a34aa99
Tim Shaker <tim@tmshkr.com>
2023-04-21 15:16:01 -0700

    progress

--- packages/mailer/index.ts
+++ packages/mailer/index.ts
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 require("dotenv").config({ path: "../../.env" });
 
-export async function sendWelcomeEmail(emailTo: string, root: string) {
+export async function sendWelcomeEmail(emailTo: string, root?: string) {
   const email = new Email({
     message: {
       from: "hi@journaling.place",
@@ -16,6 +16,16 @@ export async function sendWelcomeEmail(emailTo: string, root: string) {
       openSimulator: false,
     },
     views: { root },
+    juice: true,
+    juiceSettings: {
+      tableElements: ["TABLE"],
+    },
+    juiceResources: {
+      applyStyleTags: true,
+      webResources: {
+        relativeTo: path.resolve("styles"),
+      },
+    },
   });
 
   email
@@ -55,20 +65,7 @@ export async function sendPromptOfTheDay(emailTo: string) {
     juiceResources: {
       applyStyleTags: true,
       webResources: {
-        //
-        // this is the relative directory to your CSS/image assets
-        // and its default path is `build/`:
-        //
-        // e.g. if you have the following in the `<head`> of your template:
-        // `<link rel="stylesheet" href="style.css" data-inline="data-inline">`
-        // then this assumes that the file `build/style.css` exists
-        //
         relativeTo: path.resolve("styles"),
-        //
-        // but you might want to change it to something like:
-        // relativeTo: path.join(__dirname, '..', 'assets')
-        // (so that you can re-use CSS/images that are used in your web-app)
-        //
       },
     },
   });
