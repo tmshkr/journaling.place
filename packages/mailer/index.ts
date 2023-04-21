752287d3824cadd84d44077b44f1d53d2c6449e6
Tim Shaker <tim@tmshkr.com>
2023-04-20 19:19:00 -0700

    sendPromptOfTheDay

--- packages/mailer/index.ts
+++ packages/mailer/index.ts
@@ -1,8 +1,10 @@
 import Email from "email-templates";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const nodemailer = require("nodemailer");
-// require("dotenv").config({ path: "../../.env" });
+require("dotenv").config({ path: "../../.env" });
 
-export async function sendWelcomEmail(emailTo, root) {
+export async function sendWelcomeEmail(emailTo: string, root: string) {
   const email = new Email({
     message: {
       from: "hi@journaling.place",
@@ -27,3 +29,42 @@ export async function sendWelcomEmail(emailTo, root) {
     })
     .catch(console.error);
 }
+
+export async function sendPromptOfTheDay(emailTo: string, root?: string) {
+  const count = await prisma.prompt.count();
+  const [randomPrompt] = await prisma.prompt.findMany({
+    take: 1,
+    select: { id: true, text: true },
+    skip: Math.floor(Math.random() * count),
+  });
+
+  const email = new Email({
+    message: {
+      from: "hi@journaling.place",
+    },
+    send: false,
+    transport: nodemailer.createTransport(process.env.EMAIL_SERVER),
+    preview: {
+      openSimulator: false,
+    },
+    views: { root },
+  });
+
+  email
+    .send({
+      template: "potd",
+      message: {
+        to: emailTo,
+      },
+      locals: {
+        prompt: randomPrompt,
+        url: process.env.NEXTAUTH_URL,
+      },
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(console.error);
+}
+
+sendPromptOfTheDay(process.env.TEST_EMAIL_TO as string);
