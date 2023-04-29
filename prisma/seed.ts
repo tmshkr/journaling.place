b1410630041b0e4ef4388df4bc88248d9838292c
Tim Shaker <tim@tmshkr.com>
2023-04-28 23:36:40 -0700

    update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,73 +2,22 @@ import { PrismaClient } from "@prisma/client";
 const prompts = require("../data/prompts.json");
 const prisma = new PrismaClient();
 
-//Note: put new prompts at beginning of the JSON array
 async function seed() {
   let newPrompts = 0;
-  let newTags = 0;
 
-  for (const prompt of prompts) {
-    const count = await createPrompt(prompt);
-    if (count.newPrompt === 0) break;
+  for (const { prompt, tags } of prompts) {
+    await prisma.prompt.create({
+      data: {
+        text: prompt,
+        tags,
+      },
+    });
+    console.log(`Created prompt: ${prompt}`);
 
-    newPrompts += count.newPrompt;
-    newTags += count.newTags;
+    newPrompts++;
   }
 
   console.log(`Created ${newPrompts} new prompts`);
-  console.log(`Created ${newTags} new tags`);
-}
-
-async function createPrompt({ prompt, tags }) {
-  const count = { newTags: 0, newPrompt: 0 };
-  return prisma.$transaction(async (tx) => {
-    const { id: promptId } = await tx.prompt
-      .create({
-        data: {
-          text: prompt,
-        },
-      })
-      .catch((err) => {
-        if (err.code === "P2002") {
-          console.log("prompt already exists: ", prompt);
-          return { id: null };
-        }
-        console.error(err);
-        throw err;
-      });
-
-    if (promptId === null) return count;
-    count.newPrompt++;
-
-    for (const tag of tags) {
-      const { id: tagId } = await tx.tag
-        .findUniqueOrThrow({
-          where: { text: tag },
-        })
-        .catch(async (err) => {
-          if (err.code === "P2025") {
-            console.log("creating new tag: ", tag);
-            const newTag = await tx.tag.create({
-              data: {
-                text: tag,
-              },
-            });
-            count.newTags++;
-            return newTag;
-          }
-          console.error(err);
-          throw err;
-        });
-      await tx.tagOnPrompt.create({
-        data: {
-          promptId,
-          tagId,
-        },
-      });
-    }
-
-    return count;
-  });
 }
 
 seed()
