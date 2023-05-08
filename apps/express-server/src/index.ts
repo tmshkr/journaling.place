d816ed986384ae6eca2209caf292f24e18c6028d
Tim Shaker <tim@tmshkr.com>
2023-05-08 16:30:06 -0700

    add socketio

---
+++ apps/express-server/src/index.ts
@@ -0,0 +1,37 @@
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: "../../.env" });
+}
+
+import express from "express";
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(8888, () => {
+  console.log("listening on *:8888");
+});
