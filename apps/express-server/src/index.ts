00cf47436c2f730df636b532fc313d3245b50452
Tim Shaker <tim@tmshkr.com>
2023-05-28 09:38:25 -0700

    cookie middleware

--- apps/express-server/src/index.ts
+++ apps/express-server/src/index.ts
@@ -3,6 +3,11 @@ if (process.env.NODE_ENV === "development") {
 }
 
 import express from "express";
+import { getToken } from "next-auth/jwt";
+import { PrismaClient } from "@prisma/client";
+
+const cookie = require("cookie");
+const prisma: PrismaClient = new PrismaClient();
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
@@ -13,20 +18,36 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+io.use(async (socket, next) => {
+  try {
+    const req: any = {
+      cookies: cookie.parse(socket.request.headers.cookie),
+      headers: socket.request.headers,
+    };
+    const token = await getToken({ req });
+    if (token) {
+      socket.request.token = token;
+      console.log(socket.request.token);
+      next();
+    } else {
+      next(new Error("Authentication error"));
+    }
+  } catch {
+    next(new Error("Authentication error"));
+  }
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-});
 
-io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
   });
-});
 
-io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
