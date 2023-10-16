5a72b7bec80382ba37e6d75a298185288f6e57c0
Tim Shaker <tim@tmshkr.com>
2023-10-16 13:55:56 -0700

    add types

--- apps/web/src/components/OtherEntries.tsx
+++ apps/web/src/components/OtherEntries.tsx
@@ -1,52 +1,57 @@
 import Link from "next/link";
 import { useQueryClient } from "react-query";
+import { JournalCache, CachedJournal } from "src/store/journal";
 
 import dayjs from "src/lib/dayjs";
 
 export function OtherEntries({ prompt, journal }) {
+  if (!journal || !prompt) return null;
   const queryClient = useQueryClient();
-  const cache: any = queryClient.getQueryData("journal");
+  const cache = queryClient.getQueryData<JournalCache>("journal");
   if (!cache) return null;
 
   const { journalsById, journalsByPromptId } = cache;
-  let journals: any = [];
+  const relatedJournals: CachedJournal[] = [];
 
-  if (journalsByPromptId[prompt?.id]) {
+  if (journalsByPromptId[prompt.id]) {
     for (const journalId of journalsByPromptId[prompt.id]) {
-      if (journalId != journal?.id && journal?.status === "ACTIVE") {
-        journals.push(journalsById[journalId]);
+      if (journalId !== journal.id) {
+        relatedJournals.push(journalsById[journalId]);
       }
     }
   }
 
-  if (journals.length === 0) return null;
-  journals = journals.sort((a, b) => {
-    return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
-  });
+  if (relatedJournals.length === 0) return null;
 
   return (
     <div className="my-12">
       <h3 className="neuton text-center my-3 text-lg">More Responses</h3>
       <ul role="list" className="divide-y divide-gray-200">
-        {journals.map((journal) => {
-          return journal.status === "ACTIVE" ? (
-            <li key={journal.id}>
-              <Link
-                href={`/journal/${journal.id}`}
-                className="block hover:bg-gray-50"
-              >
-                <div className="p-5">
-                  <p className="truncate text-sm text-gray-600">
-                    {journal.plaintext}
-                  </p>
-                  <p className="text-sm text-gray-500 mt-2">
-                    {dayjs(journal.updatedAt).format("MMM D h:mm A")}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          ) : null;
-        })}
+        {relatedJournals
+          .sort((a, b) => {
+            return (
+              new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+            );
+          })
+          .map((journal) => {
+            return journal.status === "ACTIVE" ? (
+              <li key={journal.id}>
+                <Link
+                  href={`/journal/${journal.id}`}
+                  className="block hover:bg-gray-50"
+                >
+                  <div className="p-5">
+                    <p className="truncate text-sm text-gray-600">
+                      {journal.plaintext}
+                    </p>
+                    <p className="text-sm text-gray-500 mt-2">
+                      {dayjs(journal.updatedAt).format("MMM D h:mm A")}
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            ) : null;
+          })}
       </ul>
     </div>
   );
