5e5263500b7bd5145873dee6411c510c7474fa7b
Tim Shaker <tim@tmshkr.com>
2023-10-15 19:11:47 -0700

    delete handlePut

--- apps/web/src/pages/api/journal/[id]/index.ts
+++ apps/web/src/pages/api/journal/[id]/index.ts
@@ -2,10 +2,9 @@ import { withUser } from "src/middleware/withUser";
 import { prisma } from "src/lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
-import { z } from "zod";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
-router.use(withUser).get(handleGet).put(handlePut);
+router.use(withUser).get(handleGet);
 
 async function handleGet(req, res) {
   const journal = await prisma.journal.findUnique({
@@ -30,40 +29,6 @@ async function handleGet(req, res) {
   }
 
   return res.json(journal);
-}
-
-async function handlePut(req, res) {
-  try {
-    z.object({
-      ciphertext: z.object({
-        type: z.string().regex(/Buffer/),
-        data: z.array(z.number()),
-      }),
-      iv: z.object({
-        type: z.string().regex(/Buffer/),
-        data: z.array(z.number()),
-      }),
-    }).parse(req.body);
-  } catch (err) {
-    console.error(err);
-    return res.status(400).json({ errorMessage: "Bad Request" });
-  }
-
-  const { ciphertext, iv } = req.body;
-  await prisma.journal.update({
-    where: {
-      id_authorId: {
-        id: req.query.id,
-        authorId: req.user.id,
-      },
-    },
-    data: {
-      ciphertext: Buffer.from(ciphertext),
-      iv: Buffer.from(iv),
-    },
-  });
-
-  return res.send();
 }
 
 export default router.handler({
