c11453af95488315ed7ac2224d8b066043ac1a6a
Tim Shaker <tim@tmshkr.com>
2023-10-18 22:56:34 -0700

    update import

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -5,7 +5,7 @@ import {
   wsLink,
 } from "@trpc/client";
 
-import type { AppRouter } from "trpc-server/src/index";
+import type { AppRouter } from "trpc-server/src/router";
 
 const trpcUrl = process.env.NEXTAUTH_URL!.replace(":3000", ":2222") + "/trpc";
 
