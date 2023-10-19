6b68f02ae2072b269ad650825ca0786041367e6a
Tim Shaker <tim@tmshkr.com>
2023-10-18 17:08:46 -0700

    delete trpc/next

--- apps/web/src/server/routers/_app.ts
+++
@@ -1,9 +0,0 @@
-import { router } from "../trpc";
-import { journalRouter } from "./journal/journalRouter";
-
-export const appRouter = router({
-  journal: journalRouter,
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
