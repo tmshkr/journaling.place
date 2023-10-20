df8aed157039f319d943cf4df89f9e053136512b
Tim Shaker <tim@tmshkr.com>
2023-10-19 18:42:13 -0700

    use ws server

--- apps/trpc-server/src/server.ts
+++ apps/trpc-server/src/server.ts
@@ -1,19 +1,27 @@
-import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import { applyWSSHandler } from "@trpc/server/adapters/ws";
-import { WebSocketServer } from "ws";
+import ws from "ws";
 import { appRouter, AppRouter } from "./router";
 import { createContext } from "./context";
 
-const { server, listen } = createHTTPServer({
-  router: appRouter,
-  createContext,
-});
+const port = 2222;
 
-const wss = new WebSocketServer({ server });
-applyWSSHandler<AppRouter>({
+const wss = new ws.Server({ port });
+const handler = applyWSSHandler<AppRouter>({
   wss,
   router: appRouter,
   createContext,
 });
 
-listen(2222);
+wss.on("connection", (ws) => {
+  console.log(`➕➕ Connection (${wss.clients.size})`);
+  ws.once("close", () => {
+    console.log(`➖➖ Connection (${wss.clients.size})`);
+  });
+});
+
+console.log(`✅ WebSocket Server listening on ws://localhost:${port}`);
+process.on("SIGTERM", () => {
+  console.log("SIGTERM");
+  handler.broadcastReconnectNotification();
+  wss.close();
+});
