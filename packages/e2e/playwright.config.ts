fda0e62ab9008750eba45fbafd31690f3ccfa60f
Tim Shaker <tim@tmshkr.com>
2023-11-04 14:58:16 -0700

    fix e2e

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -1,11 +1,6 @@
+require("dotenv").config({ path: "../../.env" });
 import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-require("dotenv").config({ path: "../../.env" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
