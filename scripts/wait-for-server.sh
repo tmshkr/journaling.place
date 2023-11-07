7ea6db9fa6484d04b598aae3143d0a51de08282a
Tim Shaker <tim@tmshkr.com>
2023-11-07 14:54:12 -0800

    update wait-for-server

--- scripts/wait-for-server.sh
+++ scripts/wait-for-server.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-while ! curl --output /dev/null --silent --head --fail $1 -k; do
-  echo '.'
+while true; do
+  version=$(curl --silent --fail -k $1/api/info | jq -r '.version')
+  if [ $version == $2 ]; then
+    echo "$1 running version $version"
+    break
+  else
+    echo "Waiting for $1 to be ready"
+  fi
   sleep 5
 done
