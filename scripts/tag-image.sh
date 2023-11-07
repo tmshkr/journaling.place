a11b836de851a6a07e27318061e3c5f72e1643c2
Tim Shaker <tim@tmshkr.com>
2023-11-07 11:44:01 -0800

    update script

--- scripts/tag-image.sh
+++ scripts/tag-image.sh
@@ -3,4 +3,4 @@ set -e
 
 MANIFEST=$(aws ecr batch-get-image --repository-name journaling.place --image-ids imageTag=$1 --output json | jq --raw-output --join-output '.images[0].imageManifest')
 
-aws ecr put-image --repository-name journaling.place --image-tag $BRANCH --image-manifest "$MANIFEST"
+aws ecr put-image --repository-name journaling.place --image-tag $2 --image-manifest "$MANIFEST"
