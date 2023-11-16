9384adc1969cabaa1463cf8e0a86b04aedd4e55a
Tim Shaker <tim@tmshkr.com>
2023-11-15 20:11:04 -0800

    turbo caching

--- scripts/prune-modules.sh
+++ scripts/prune-modules.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir -p saved_modules
 cd node_modules
-mv .bin *prisma turbo ../saved_modules/
+mv .bin .cache *prisma turbo ../saved_modules/
 cd ..
 rm -rf node_modules
 mv saved_modules node_modules
