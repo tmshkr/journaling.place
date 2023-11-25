2d26bc126ad7142ed0420cd514ba8af2805ca8ad
Tim Shaker <tim@tmshkr.com>
2023-11-25 15:16:16 -0800

    mkdir ~/.ssh

--- scripts/stop-test-server.sh
+++ scripts/stop-test-server.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -e
 
 echo "Setting up SSH Client"
+mkdir -m 700 $HOME/.ssh
 echo "$SSH_HOST_PUBLIC_KEY" >>$HOME/.ssh/known_hosts
 echo "$SSH_CLIENT_PUBLIC_KEY" >>$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >>$HOME/.ssh/id_rsa
