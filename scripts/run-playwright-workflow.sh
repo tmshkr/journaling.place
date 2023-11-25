8f71a89e832d68b0ca6fa084a6626e54df018a3b
Tim Shaker <tim@tmshkr.com>
2023-11-24 21:49:42 -0800

    add run-playwright-workflow.sh

---
+++ scripts/run-playwright-workflow.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -e
+
+BASE_URL=$(echo $NGROK_TUNNELS | jq -r '.[] | select(.name == "web") | .public_url')
+mongo_url=$(echo $NGROK_TUNNELS | jq -r '.[] | select(.name == "mongo") | .public_url')
+
+# parse tcp url
+hostname=$(echo $mongo_url | cut -d'/' -f3 | cut -d':' -f1)
+port=$(echo $mongo_url | cut -d':' -f3)
+echo "hostname: $hostname"
+echo "port: $port"
+
+MONGO_URI="mongodb://$MONGO_USER:$MONGO_PASSWORD@$hostname:$port/jp-test?authSource=admin&directConnection=true"
+
+echo "BASE_URL: $BASE_URL"
+echo "MONGO_URI: $MONGO_URI"
+
+gh workflow run playwright-test.yaml \
+  -f BASE_URL=$BASE_URL \
+  -f MONGO_URI=$MONGO_URI \
+  -f SSH_HOST_PUBLIC_KEY=$SSH_HOST_PUBLIC_KEY \
+  --ref $GITHUB_REF_NAME
