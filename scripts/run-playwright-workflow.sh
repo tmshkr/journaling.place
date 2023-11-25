cf37d833effe7ff1ccbb479ff3c16fdd4997f066
Tim Shaker <tim@tmshkr.com>
2023-11-25 11:21:20 -0800

    output BASE_URL

--- scripts/run-playwright-workflow.sh
+++ scripts/run-playwright-workflow.sh
@@ -6,16 +6,13 @@ mongo_url=$(echo $NGROK_TUNNELS | jq -r '.[] | select(.name == "mongo") | .publi
 # parse tcp url
 hostname=$(echo $mongo_url | cut -d'/' -f3 | cut -d':' -f1)
 port=$(echo $mongo_url | cut -d':' -f3)
-echo "hostname: $hostname"
-echo "port: $port"
-
 MONGO_URI="mongodb://$MONGO_USER:$MONGO_PASSWORD@$hostname:$port/jp-test?authSource=admin&tls=false&directConnection=true"
-
-echo "BASE_URL: $BASE_URL"
-echo "MONGO_URI: $MONGO_URI"
 
 gh workflow run playwright-test.yaml \
   -f BASE_URL=$BASE_URL \
   -f MONGO_URI=$MONGO_URI \
   -f SSH_HOST_PUBLIC_KEY=$SSH_HOST_PUBLIC_KEY \
   --ref $GITHUB_REF_NAME
+
+# Set Output
+echo "BASE_URL=$BASE_URL" >>$GITHUB_OUTPUT
