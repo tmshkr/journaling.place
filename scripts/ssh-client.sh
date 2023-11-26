ec14d45390245db431c98749f19fb7bf8f18ccd4
Tim Shaker <tim@tmshkr.com>
2023-11-25 16:43:04 -0800

    hash known_hosts

--- scripts/ssh-client.sh
+++ scripts/ssh-client.sh
@@ -3,7 +3,7 @@
 echo "Setting up SSH Client"
 mkdir -m 700 $HOME/.ssh
 ssh-keyscan -p $SSH_PORT $SSH_HOSTNAME >>$HOME/.ssh/known_hosts
-cat $HOME/.ssh/known_hosts
+ssh-keygen -Hf $HOME/.ssh/known_hosts
 echo "$SSH_CLIENT_PUBLIC_KEY" >>$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >>$HOME/.ssh/id_rsa
 
