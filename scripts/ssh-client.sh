0c4f5ba3687716b0ca9e276ccde7127949b28d20
Tim Shaker <tim@tmshkr.com>
2023-11-25 17:01:41 -0800

    update permissions

--- scripts/ssh-client.sh
+++ scripts/ssh-client.sh
@@ -1,10 +1,11 @@
 #! /bin/bash -e
 
 echo "Setting up SSH Client"
-mkdir -m 700 $HOME/.ssh
+mkdir -m 600 $HOME/.ssh
 ssh-keyscan -p $SSH_PORT $SSH_HOSTNAME >>$HOME/.ssh/known_hosts
 ssh-keygen -Hf $HOME/.ssh/known_hosts
 echo "$SSH_CLIENT_PUBLIC_KEY" >>$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >>$HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/*
 
 ssh -o StrictHostKeychecking=no -i $HOME/.ssh/id_rsa -p $SSH_PORT $SSH_USER@$SSH_HOSTNAME $1
