8942c7f81ccfd60ae55df70d8effc9c660f3cdde
Tim Shaker <tim@tmshkr.com>
2023-11-25 20:26:16 -0800

    fix ssh command

--- scripts/ssh-client.sh
+++ scripts/ssh-client.sh
@@ -8,6 +8,7 @@ echo "$SSH_CLIENT_PUBLIC_KEY" >>$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >>$HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/*
 
-ssh -o StrictHostKeychecking=no -i $HOME/.ssh/id_rsa -p 16248 runner@6.tcp.us-cal-1.ngrok.io pwd
+ssh -o StrictHostKeychecking=no -p $SSH_PORT $SSH_USER@$SSH_HOSTNAME pwd
 
 ls -al /etc/ssh
+ls -al $HOME/.ssh
