71e9d12ecbc69b0f23c5c8712276928bf74f9b99
Tim Shaker <tim@tmshkr.com>
2023-11-25 22:14:38 -0800

    use ssh_config

--- scripts/setup-ssh-client.sh
+++ scripts/setup-ssh-client.sh
@@ -1,7 +1,18 @@
 #! /bin/bash -e
 
 echo "Setting up SSH client..."
+
+ssh_config="
+Host $SSH_HOSTNAME
+  HostName $SSH_HOSTNAME
+  User $SSH_USER
+  Port $SSH_PORT
+  IdentityFile $HOME/.ssh/id_rsa
+  StrictHostKeyChecking no
+"
+
 mkdir -m 600 $HOME/.ssh
 echo "$SSH_CLIENT_PUBLIC_KEY" >>$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >>$HOME/.ssh/id_rsa
+echo "$ssh_config" >>$HOME/.ssh/config
 chmod 600 $HOME/.ssh/*
--- scripts/stop-test-server.sh
+++ scripts/stop-test-server.sh
@@ -2,4 +2,4 @@
 
 echo "Stopping test server..."
 
-ssh -o StrictHostKeychecking=no -i $HOME/.ssh/id_rsa -p $SSH_PORT $SSH_USER@$SSH_HOSTNAME docker compose -f /home/runner/work/journaling.place/journaling.place/docker-compose.test.yml down
+ssh -F $HOME/.ssh/config $SSH_HOSTNAME docker compose -f /home/runner/work/journaling.place/journaling.place/docker-compose.test.yml down
