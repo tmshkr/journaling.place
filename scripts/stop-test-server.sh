eb3800973bffd860bb83cd5b8d60331df08b74b0
Tim Shaker <tim@tmshkr.com>
2023-11-26 10:14:14 -0800

    debug

--- scripts/stop-test-server.sh
+++ scripts/stop-test-server.sh
@@ -3,3 +3,7 @@
 echo "Stopping test server..."
 
 ssh -F $HOME/.ssh/config $SSH_HOSTNAME docker compose -f /home/runner/work/journaling.place/journaling.place/docker-compose.test.yml down
+
+ls -la $HOME/.ssh
+cat $HOME/.ssh/config
+cat $HOME/.ssh/known_hosts
