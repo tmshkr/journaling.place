5ad239f64f7af9016384611931d5dce3f1043ec4
Tim Shaker <tim@tmshkr.com>
2023-11-25 17:12:14 -0800

    ssh config

--- scripts/ssh-client.sh
+++ scripts/ssh-client.sh
@@ -2,10 +2,12 @@
 
 echo "Setting up SSH Client"
 mkdir -m 600 $HOME/.ssh
-ssh-keyscan -p $SSH_PORT $SSH_HOSTNAME >>$HOME/.ssh/known_hosts
-ssh-keygen -Hf $HOME/.ssh/known_hosts
+# ssh-keyscan -p $SSH_PORT $SSH_HOSTNAME >>$HOME/.ssh/known_hosts
+# ssh-keygen -Hf $HOME/.ssh/known_hosts
 echo "$SSH_CLIENT_PUBLIC_KEY" >>$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >>$HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/*
 
 ssh -o StrictHostKeychecking=no -i $HOME/.ssh/id_rsa -p $SSH_PORT $SSH_USER@$SSH_HOSTNAME $1
+
+cat $HOME/.ssh/known_hosts
