a1d8c41a43ee81ab5c7a65ecae7f0d0f74d4ffa1
Tim Shaker <tim@tmshkr.com>
2023-11-27 20:23:48 -0800

    rename workflow

--- scripts/run-playwright-workflow.sh
+++ scripts/run-playwright-workflow.sh
@@ -8,7 +8,7 @@ hostname=$(echo $mongo_url | cut -d'/' -f3 | cut -d':' -f1)
 port=$(echo $mongo_url | cut -d':' -f3)
 MONGO_URI="mongodb://$MONGO_USER:$MONGO_PASSWORD@$hostname:$port/jp-test?authSource=admin&tls=false&directConnection=true"
 
-gh workflow run playwright-test.yaml \
+gh workflow run playwright.yaml \
   -f BASE_URL=$BASE_URL \
   -f MONGO_URI=$MONGO_URI \
   -f SSH_HOSTNAME=$SSH_HOSTNAME \
