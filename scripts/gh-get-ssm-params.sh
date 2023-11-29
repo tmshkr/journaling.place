6f59d8e6abeb0feb82787db0c52988020bddaa04
Tim Shaker <tim@tmshkr.com>
2023-11-28 18:44:38 -0800

    update gh-get-ssm-params

--- scripts/gh-get-ssm-params.sh
+++ scripts/gh-get-ssm-params.sh
@@ -7,6 +7,8 @@ fi
 
 # Retrieve parameters from AWS SSM
 aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
-  echo "$(echo $name | sed "s/\/journaling.place\/$ENVIRONMENT\///")=$value" >>$GITHUB_ENV
+  echo "$(echo $name | sed "s/\/journaling.place\/$ENVIRONMENT\///")=$value" >>.env
   echo "::add-mask::$value"
 done
+
+cat .env
