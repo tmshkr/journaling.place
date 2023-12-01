f834728a57ae43b6c09a5f0207f7eef3e55f38f9
Tim Shaker <tim@tmshkr.com>
2023-11-30 22:36:45 -0800

    update deploy.yaml

--- .platform/hooks/prebuild/get-env-vars.sh
+++ .platform/hooks/prebuild/get-env-vars.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 
-# get environment
-ENVIRONMENT=$(cat ENVIRONMENT)
-SHA=$(cat SHA)
-
 # Retrieve parameters from AWS SSM
 aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
   echo "$(echo $name | sed "s/\/journaling.place\/$ENVIRONMENT\///")=$value" >>.env
 done
-
-echo "ENVIRONMENT=$ENVIRONMENT" >>.env
-echo "SHA=$SHA" >>.env
-echo "APP_VERSION=$ENVIRONMENT-$SHA" >>.env
