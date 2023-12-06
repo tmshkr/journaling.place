3187e61d36a26b08c2626905a04048b8e8b78dab
Tim Shaker <tim@tmshkr.com>
2023-12-06 15:24:56 -0800

    update config

--- .platform/hooks/prebuild/get-env-vars.sh
+++ .platform/hooks/prebuild/get-env-vars.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-ENVIRONMENT=$(/opt/elasticbeanstalk/bin/get-config environment -k ENVIRONMENT)
-
-echo "ENVIRONMENT=$ENVIRONMENT" >>.env
+source .env
 
 # Retrieve parameters from AWS SSM
 aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
