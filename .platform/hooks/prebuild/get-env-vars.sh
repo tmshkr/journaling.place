ac92aa5c5ff2307abb0cbf47055334de657a7744
Tim Shaker <tim@tmshkr.com>
2023-12-06 22:40:31 -0800

    update get env vars

--- .platform/hooks/prebuild/get-env-vars.sh
+++ .platform/hooks/prebuild/get-env-vars.sh
@@ -2,7 +2,10 @@
 set -e
 
 ENVIRONMENT=$(cat env.json | jq -r '.ENVIRONMENT')
-IS_PRODUCTION=$(/opt/elasticbeanstalk/bin/get-config environment -k IS_PRODUCTION) || false
+IS_PRODUCTION=$(/opt/elasticbeanstalk/bin/get-config environment -k IS_PRODUCTION)
+if [ $? -ne 0 ]; then
+  IS_PRODUCTION=false
+fi
 
 # Retrieve parameters from AWS SSM
 aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
