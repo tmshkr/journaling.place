6626ed50d8f5b88179a33dc2f1fb8dcbfa866d13
Tim Shaker <tim@tmshkr.com>
2023-12-06 16:07:52 -0800

    update env config

--- .platform/confighooks/prebuild/get-env-vars.sh
+++ .platform/confighooks/prebuild/get-env-vars.sh
@@ -6,7 +6,9 @@ source .env
 # Retrieve parameters from AWS SSM
 aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
   key=$(echo $name | sed "s/\/journaling.place\/$ENVIRONMENT\///")
-  if ! cat .env | grep -q "$key"; then
-    echo "$key=$value" >>test.env
+  if cat .env | grep -q "$key"; then
+    sed -i "s/$key=.*/$key=$value/" .env
+  else
+    echo "$key=$value" >>.env
   fi
 done
