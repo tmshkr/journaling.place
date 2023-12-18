ac1791a32f6ae8d5a13b776fbc40c483cde3c975
Tim Shaker <tim@tmshkr.com>
2023-12-18 15:10:39 -0800

    update script

--- packages/aws/setup/efs-create-volume.ts
+++ packages/aws/setup/efs-create-volume.ts
@@ -1,4 +1,5 @@
 import { EC2Client, DescribeSubnetsCommand } from "@aws-sdk/client-ec2";
+import { SSMClient, PutParameterCommand } from "@aws-sdk/client-ssm";
 import {
   EFSClient,
   CreateFileSystemCommand,
@@ -6,6 +7,7 @@ import {
 } from "@aws-sdk/client-efs";
 const efsClient = new EFSClient();
 const ec2Client = new EC2Client();
+const ssmClient = new SSMClient();
 
 async function run() {
   const { FileSystemId } = await efsClient
@@ -16,22 +18,50 @@ async function run() {
         Encrypted: true,
       })
     )
+    .then(({ FileSystemId }) => {
+      console.log(`Created EFS volume ${FileSystemId}`);
+      return { FileSystemId };
+    })
     .catch((err) => {
       if (err.name === "FileSystemAlreadyExists") {
+        console.log(`EFS volume already exists ${err.FileSystemId}`);
         return { FileSystemId: err.FileSystemId };
       } else throw err;
     });
 
   const { Subnets } = await ec2Client.send(new DescribeSubnetsCommand({}));
   for (const subnet of Subnets) {
-    console.log(`Creating mount target in ${subnet.AvailabilityZone}`);
-    await efsClient.send(
-      new CreateMountTargetCommand({
-        FileSystemId,
-        SubnetId: subnet.SubnetId,
+    await efsClient
+      .send(
+        new CreateMountTargetCommand({
+          FileSystemId,
+          SubnetId: subnet.SubnetId,
+        })
+      )
+      .then(() => {
+        console.log(`Mount target created in ${subnet.AvailabilityZone}`);
       })
-    );
+      .catch((err) => {
+        if (err.name === "MountTargetConflict") {
+          console.log(
+            `Mount target already exists in ${subnet.AvailabilityZone}`
+          );
+        } else throw err;
+      });
   }
+
+  await ssmClient
+    .send(
+      new PutParameterCommand({
+        Name: "/journaling.place/EFS_VOLUME_ID",
+        Value: FileSystemId,
+        Type: "String",
+        Overwrite: true,
+      })
+    )
+    .then(() => {
+      console.log("SSM parameter created");
+    });
 }
 
 run();
