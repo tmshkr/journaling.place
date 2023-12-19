a96b88d35814e0c055860133b9faa8ec62bfba7c
Tim Shaker <tim@tmshkr.com>
2023-12-18 22:13:28 -0800

    wip

--- packages/aws/cdk/index.ts
+++ packages/aws/cdk/index.ts
@@ -1,7 +1,5 @@
 import * as cdk from "aws-cdk-lib";
 import { StorageStack } from "./stacks/storage";
-import { IamStack } from "./stacks/iam";
 
 const app = new cdk.App();
 new StorageStack(app, "StorageStack");
-new IamStack(app, "IamStack");
--- packages/aws/setup/cfn/cfn-deploy.sh
+++ packages/aws/setup/cfn/cfn-deploy.sh
@@ -5,3 +5,7 @@ aws cloudformation deploy \
   --template-file beanstalk-iam.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides SubjectClaimFilters="repo:tmshkr/journaling.place:*"
+
+aws cloudformation deploy \
+  --stack-name s3-buckets \
+  --template-file s3-buckets.yml
