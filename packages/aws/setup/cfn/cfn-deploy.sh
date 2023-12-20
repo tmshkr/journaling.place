faa00599b7a175aa733868cfd09b4289d276f124
Tim Shaker <tim@tmshkr.com>
2023-12-19 16:29:38 -0800

    update cfn-deploy

--- packages/aws/setup/cfn/cfn-deploy.sh
+++ packages/aws/setup/cfn/cfn-deploy.sh
@@ -5,7 +5,3 @@ aws cloudformation deploy \
   --template-file beanstalk-iam.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides SubjectClaimFilters="repo:tmshkr/journaling.place:*"
-
-aws cloudformation deploy \
-  --stack-name s3-buckets \
-  --template-file s3-buckets.yml
