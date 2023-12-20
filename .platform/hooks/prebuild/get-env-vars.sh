b441126f259437dc10efdf950dd262c0dd693b5d
Tim Shaker <tim@tmshkr.com>
2023-12-20 13:28:45 -0800

    update get-env-vars

--- .platform/hooks/prebuild/get-env-vars.sh
+++ .platform/hooks/prebuild/get-env-vars.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-ENVIRONMENT=$(cat env.json | jq -r '.ENVIRONMENT')
-IS_PRODUCTION=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.IS_PRODUCTION')
+/opt/elasticbeanstalk/bin/get-config environment | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
+  echo $line >>.env
+done
+
+source .env
 
 # Retrieve parameters from AWS SSM
 aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
   key=$(echo $name | sed "s/\/journaling.place\/$ENVIRONMENT\///")
   echo "$key=$value" >>.env
 done
-
-echo "APP_VERSION=$(cat env.json | jq -r '.APP_VERSION')" >>.env
-echo "ENVIRONMENT=$(cat env.json | jq -r '.ENVIRONMENT')" >>.env
-echo "TAG=$(cat env.json | jq -r '.TAG')" >>.env
 
 if [[ "$ENVIRONMENT" == "main" && "$IS_PRODUCTION" != "true" ]]; then
   printf "\n# pre-production vars from ssm\n\n" >>.env
