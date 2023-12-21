a2550941316cd120a98bd77c7b263343f30aef17
Tim Shaker <tim@tmshkr.com>
2023-12-20 16:09:05 -0800

    update env

--- .platform/hooks/prebuild/docker-login.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Logging into Docker..."
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 983436253905.dkr.ecr.us-west-2.amazonaws.com
