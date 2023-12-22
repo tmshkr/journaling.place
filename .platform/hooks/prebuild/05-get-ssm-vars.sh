ad593213b768b5a45939f695e4ffee37a02991c5
Tim Shaker <tim@tmshkr.com>
2023-12-21 18:11:47 -0800

    add ngrok script

--- .platform/hooks/prebuild/05-get-ssm-vars.sh
+++ .platform/hooks/prebuild/05-get-ssm-vars.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-set -e
-
-/opt/elasticbeanstalk/bin/get-config environment | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
-  echo $line >>.env
-done
+#!/bin/bash -e
 
 source .env
 
@@ -12,7 +7,3 @@ aws ssm get-parameters-by-path --path "/journaling.place/$ENVIRONMENT/" --recurs
   key=$(echo $name | sed "s/\/journaling.place\/$ENVIRONMENT\///")
   echo "$key=$value" >>.env
 done
-
-if [[ "$IS_PRODUCTION" != "true" ]]; then
-  echo "NEXTAUTH_URL=$NGROK_URL" >>.env
-fi
