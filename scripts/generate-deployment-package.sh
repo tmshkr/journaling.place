dbc9f09937dc85f5c522f3a6744b6125e3e7a9da
Tim Shaker <tim@tmshkr.com>
2023-12-22 15:07:21 -0800

    fix sub-option-settings.sh

--- scripts/generate-deployment-package.sh
+++ scripts/generate-deployment-package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 source sub-option-settings.sh
 
-echo $(envsubst <option-settings.json) >option-settings.json
 echo APP_VERSION=$GITHUB_REF_NAME-$TAG >>.env
 echo ENVIRONMENT=$ENVIRONMENT >>.env
 echo SHA=$GITHUB_SHA >>.env
--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
 export SSL_CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='staging.journaling.place'].CertificateArn" --output text)
+
+echo $(envsubst <option-settings.json) >option-settings.json
