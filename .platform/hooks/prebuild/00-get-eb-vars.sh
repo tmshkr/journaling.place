c581f38417cd57b3d346d6c91dbf2cc1e87afbd1
Tim Shaker <tim@tmshkr.com>
2023-12-24 10:09:13 -0800

    use DOMAIN_NAME var

--- .platform/hooks/prebuild/00-get-eb-vars.sh
+++ .platform/hooks/prebuild/00-get-eb-vars.sh
@@ -3,3 +3,7 @@
 /opt/elasticbeanstalk/bin/get-config environment | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
   echo $line >>.env
 done
+
+source .env
+
+echo "NEXTAUTH_URL=https://$DOMAIN_NAME" >>.env
