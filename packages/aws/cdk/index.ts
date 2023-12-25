5adf8b40f5fca106937b3b81941c2b2c306e7458
Tim Shaker <tim@tmshkr.com>
2023-12-25 15:36:56 -0800

    ssh ingress

--- packages/aws/cdk/index.ts
+++ packages/aws/cdk/index.ts
@@ -1,5 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { StorageStack } from "./stacks/storage";
+import { EC2Stack } from "./stacks/ec2";
 
+const env = {
+  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+};
 const app = new cdk.App();
-new StorageStack(app, "StorageStack");
+new StorageStack(app, "StorageStack", { env });
+new EC2Stack(app, "EC2Stack", { env });
