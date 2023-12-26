884511b76da18d657ad6826b1c21ca998d2d36c6
Tim Shaker <tim@tmshkr.com>
2023-12-25 22:13:17 -0800

    use sg name

--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
 export SSL_CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='$DOMAIN_NAME'].CertificateArn" --output text)
-export DEFAULT_VPC_ID=$(aws ec2 describe-vpcs --query 'Vpcs[?IsDefault==`true`].VpcId' --output text)
-export DEFAULT_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=$DEFAULT_VPC_ID --query 'SecurityGroups[?GroupName==`default`].GroupId' --output text)
 
 echo $(envsubst <option-settings.json) >option-settings.json
