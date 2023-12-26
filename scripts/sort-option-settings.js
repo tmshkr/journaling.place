7370fa17fa0fc3047ce07a46509934357a1cf6fd
Tim Shaker <tim@tmshkr.com>
2023-12-25 17:18:42 -0800

    improve option setting sort

--- scripts/sort-option-settings.js
+++ scripts/sort-option-settings.js
@@ -3,7 +3,21 @@ const fs = require("fs");
 const path = require("path");
 const option_settings = require("../option-settings.json");
 
-option_settings.sort((a, b) => (a.Namespace < b.Namespace ? -1 : 1));
+option_settings.sort((a, b) => {
+  if (a.Namespace < b.Namespace) {
+    return -1;
+  } else if (a.Namespace > b.Namespace) {
+    return 1;
+  } else {
+    if (a.OptionName < b.OptionName) {
+      return -1;
+    } else if (a.OptionName > b.OptionName) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+});
 
 fs.writeFileSync(
   path.resolve(__dirname, "../option-settings.json"),
