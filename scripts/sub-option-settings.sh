cb4e0397068a1c6eaab758048b00ad522dfdf5df
Tim Shaker <tim@tmshkr.com>
2023-12-25 17:43:20 -0800

    get ssh access list

--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 export SSL_CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='$DOMAIN_NAME'].CertificateArn" --output text)
+export SSH_ACCESS_LIST=$(aws cloudformation describe-stacks --stack-name EC2Stack | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="SSHAccessListId") | .OutputValue')
 
 echo $(envsubst <option-settings.json) >option-settings.json
