063babaaf59c0b1851cc76cb705372593d7a79d7
Tim Shaker <tim@tmshkr.com>
2023-12-27 11:57:28 -0800

    use prep app

--- scripts/generate-deployment-package.sh
+++ scripts/generate-deployment-package.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
 source ./scripts/sub-option-settings.sh
 
+if [ $PREP == "true" ]; then
+  echo APP_VERSION=prep-$TAG >>.env
+  echo ENVIRONMENT=$ENVIRONMENT >>.env
+  echo GITHUB_REF_NAME=$GITHUB_REF_NAME >>.env
+  echo SHA=$GITHUB_SHA >>.env
+  echo TAG=$TAG >>.env
+  mv -f docker-compose.prep.yml docker-compose.yml
+  zip bundle.zip docker-compose.yml .ebextensions .env
+  exit 0
+fi
+
 echo APP_VERSION=$GITHUB_REF_NAME-$TAG >>.env
 echo ENVIRONMENT=$ENVIRONMENT >>.env
+echo GITHUB_REF_NAME=$GITHUB_REF_NAME >>.env
 echo SHA=$GITHUB_SHA >>.env
 echo TAG=$TAG >>.env
 zip -r bundle.zip . -x '*.git*'
--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-export SSL_CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='$DOMAIN_NAME'].CertificateArn" --output text)
-
 echo $(envsubst <option-settings.json) >option-settings.json
