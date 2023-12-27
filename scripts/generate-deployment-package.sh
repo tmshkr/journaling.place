0b1ffec0732723fbcb34280b7c329626721c0573
Tim Shaker <tim@tmshkr.com>
2023-12-27 12:03:45 -0800

    fix prep

--- scripts/generate-deployment-package.sh
+++ scripts/generate-deployment-package.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 source ./scripts/sub-option-settings.sh
 
-if [ $PREP == "true" ]; then
+if [ "$PREP" == "true" ]; then
   echo APP_VERSION=prep-$TAG >>.env
   echo ENVIRONMENT=$ENVIRONMENT >>.env
   echo GITHUB_REF_NAME=$GITHUB_REF_NAME >>.env
   echo SHA=$GITHUB_SHA >>.env
   echo TAG=$TAG >>.env
   mv -f docker-compose.prep.yml docker-compose.yml
-  zip bundle.zip docker-compose.yml .ebextensions .env
+  zip bundle.zip docker-compose.yml .ebextensions/* .env
   exit 0
 fi
 
