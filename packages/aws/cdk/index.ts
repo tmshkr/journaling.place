947e2714dc442c4a13a050d3c09b7df14f566159
Tim Shaker <tim@tmshkr.com>
2023-12-27 11:15:16 -0800

    add alb stack

--- packages/aws/cdk/index.ts
+++ packages/aws/cdk/index.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
-import { StorageStack } from "./stacks/storage";
+import { AlbStack } from "./stacks/alb";
 import { EC2Stack } from "./stacks/ec2";
+import { StorageStack } from "./stacks/storage";
 
 const env = {
   region: process.env.CDK_DEFAULT_REGION,
   account: process.env.CDK_DEFAULT_ACCOUNT,
 };
 const app = new cdk.App();
-new StorageStack(app, "StorageStack", { env });
+new AlbStack(app, "AlbStack", { env });
 new EC2Stack(app, "EC2Stack", { env });
+new StorageStack(app, "StorageStack", { env });
---
+++ packages/aws/cdk/stacks/alb.ts
@@ -0,0 +1,60 @@
+import * as cdk from "aws-cdk-lib";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as route53 from "aws-cdk-lib/aws-route53";
+
+export class AlbStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cert = new acm.Certificate(this, "Certificate", {
+      domainName: "journaling.place",
+      certificateName: "journaling.place",
+      subjectAlternativeNames: ["*.journaling.place"],
+      validation: acm.CertificateValidation.fromDns(
+        route53.HostedZone.fromLookup(this, "Zone", {
+          domainName: "journaling.place",
+        })
+      ),
+    });
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, "ALB", {
+      deletionProtection: true,
+      internetFacing: true,
+      ipAddressType: elbv2.IpAddressType.IPV4,
+      loadBalancerName: "jp-alb",
+      vpc: ec2.Vpc.fromLookup(this, "DefaultVPC", { isDefault: true }),
+    });
+
+    alb.addListener("HttpListener", {
+      port: 80,
+      open: true,
+      defaultAction: elbv2.ListenerAction.fixedResponse(200, {
+        contentType: "text/plain",
+        messageBody: "OK",
+      }),
+    });
+
+    alb.addListener("HttpsListener", {
+      port: 443,
+      open: true,
+      defaultAction: elbv2.ListenerAction.fixedResponse(200, {
+        contentType: "text/plain",
+        messageBody: "OK",
+      }),
+      sslPolicy: elbv2.SslPolicy.RECOMMENDED,
+      certificates: [cert],
+    });
+
+    new cdk.CfnOutput(this, "HttpsCertificateArn", {
+      value: cert.certificateArn,
+      exportName: "HttpsCertificateArn",
+    });
+
+    new cdk.CfnOutput(this, "SharedLoadBalancerArn", {
+      value: alb.loadBalancerArn,
+      exportName: "SharedLoadBalancerArn",
+    });
+  }
+}
