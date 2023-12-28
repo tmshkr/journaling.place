935c9419b46a1ed5017c87407f400faa145d76d0
Tim Shaker <tim@tmshkr.com>
2023-12-27 19:25:07 -0800

    get alb and cert arn

--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
+alb_stack=$(aws cloudformation describe-stacks --stack-name AlbStack)
+
+export SHARED_LOAD_BALANCER_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="SharedLoadBalancerArn") | .OutputValue')
+export HTTPS_CERTIFICATE_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="HttpsCertificateArn") | .OutputValue')
+
 echo $(envsubst <option-settings.json) >option-settings.json
