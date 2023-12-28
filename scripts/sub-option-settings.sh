4a678eae792a643c4c00dbcbd86288f5e6cd7967
Tim Shaker <tim@tmshkr.com>
2023-12-27 19:30:17 -0800

    check vars in sub-option-settings

--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -1,8 +1,32 @@
 #!/bin/bash -e
 
+if [ -z "$DOMAIN_NAME" ]; then
+  echo "DOMAIN_NAME is not set"
+  failed=true
+fi
+
+if [ -z "$STAGE" ]; then
+  echo "STAGE is not set"
+  failed=true
+fi
+
 alb_stack=$(aws cloudformation describe-stacks --stack-name AlbStack)
 
 export SHARED_LOAD_BALANCER_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="SharedLoadBalancerArn") | .OutputValue')
 export HTTPS_CERTIFICATE_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="HttpsCertificateArn") | .OutputValue')
+
+if [ -z "$SHARED_LOAD_BALANCER_ARN" ]; then
+  echo "SHARED_LOAD_BALANCER_ARN is not set"
+  failed=true
+fi
+
+if [ -z "$HTTPS_CERTIFICATE_ARN" ]; then
+  echo "HTTPS_CERTIFICATE_ARN is not set"
+  failed=true
+fi
+
+if [ "$failed" == true ]; then
+  exit 1
+fi
 
 echo $(envsubst <option-settings.json) >option-settings.json
