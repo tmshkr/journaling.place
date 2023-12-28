20a1592ab2dd18242091f36bf7bdc307f775b388
Tim Shaker <tim@tmshkr.com>
2023-12-28 12:37:53 -0800

    use prep var

--- .platform/hooks/prebuild/15-compose-file.sh
+++ .platform/hooks/prebuild/15-compose-file.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -e
 
 source .env
-images=$(aws ecr list-images --repository-name journaling.place)
 
-if echo $images | grep -q $TAG; then
-  echo "Image $TAG is ready"
-  cp docker-compose.run.yml docker-compose.yml
-else
-  echo "Image $TAG is not ready"
+if ["$PREP" == "true"]; then
   cp docker-compose.prep.yml docker-compose.yml
+else
+  cp docker-compose.run.yml docker-compose.yml
 fi
