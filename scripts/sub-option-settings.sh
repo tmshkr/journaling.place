c8966f6b94db542dd82ab4e8116c3ab956a4c2d6
Tim Shaker <tim@tmshkr.com>
2023-12-29 10:24:30 -0800

    update option settings

--- scripts/sub-option-settings.sh
+++ scripts/sub-option-settings.sh
@@ -3,9 +3,22 @@
 alb_stack=$(aws cloudformation describe-stacks --stack-name ALBStack)
 export SHARED_LOAD_BALANCER_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="SharedLoadBalancerArn") | .OutputValue')
 
+if [ -z "$PREP" ]; then
+  export PREP="false"
+fi
+
+if [ -z "$DOMAIN_NAME" ]; then
+  echo "DOMAIN_NAME is not set"
+  failed=true
+fi
+
 if [ -z "$SHARED_LOAD_BALANCER_ARN" ]; then
   echo "SHARED_LOAD_BALANCER_ARN is not set"
+  failed=true
+fi
+
+if [ "$failed" == true ]; then
   exit 1
 fi
 
-echo $(envsubst <option-settings.staging.json) >option-settings.staging.json
+echo $(envsubst <option-settings.json) >option-settings.staging.json
