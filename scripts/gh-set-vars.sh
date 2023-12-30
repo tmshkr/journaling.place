d83ed1c753fc77207a62d8c0388012f2f744c345
Tim Shaker <tim@tmshkr.com>
2023-12-29 16:48:46 -0800

    set alb tags with gh vars

--- scripts/gh-set-vars.sh
+++ scripts/gh-set-vars.sh
@@ -4,3 +4,16 @@ gh variable set BLUE_ENV --body "jp-blue-$key"
 gh variable set GREEN_ENV --body "jp-green-$key"
 gh variable set PRODUCTION_CNAME --body "jp-main-$key"
 gh variable set STAGING_CNAME --body "jp-staging-$key"
+
+alb_stack=$(aws cloudformation describe-stacks --stack-name ALBStack)
+prod_http_listener_rule_arn=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="prod-http-listener-rule-arn") | .OutputValue')
+prod_https_listener_rule_arn=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="prod-https-listener-rule-arn") | .OutputValue')
+staging_http_listener_rule_arn=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="staging-http-listener-rule-arn") | .OutputValue')
+staging_https_listener_rule_arn=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="staging-https-listener-rule-arn") | .OutputValue')
+
+aws elbv2 add-tags --resource-arns $prod_http_listener_rule_arn --tags Key=elasticbeanstalk:cname,Value=jp-main-$key
+aws elbv2 add-tags --resource-arns $prod_https_listener_rule_arn --tags Key=elasticbeanstalk:cname,Value=jp-main-$key
+aws elbv2 add-tags --resource-arns $staging_http_listener_rule_arn --tags Key=elasticbeanstalk:cname,Value=jp-staging-$key
+aws elbv2 add-tags --resource-arns $staging_https_listener_rule_arn --tags Key=elasticbeanstalk:cname,Value=jp-staging-$key
+
+echo "Updated listener rule tags"
