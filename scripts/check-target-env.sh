d9d3383260e499814e1e8fda2592bf7caf513e77
Tim Shaker <tim@tmshkr.com>
2024-01-02 16:59:57 -0800

    update check-target-env.sh

--- scripts/check-target-env.sh
+++ scripts/check-target-env.sh
@@ -1,6 +1,12 @@
 #! /bin/bash -e
 
 health=$(echo $TARGET_ENV_JSON | jq -r '.Health')
+cname=$(echo $TARGET_ENV_JSON | jq -r '.CNAME')
+version=$(echo $TARGET_ENV_JSON | jq -r '.VersionLabel')
+
+echo "Target environment health: $health"
+echo "Target environment version: $version"
+echo "Target environment CNAME: http://$cname"
 
 if [ "$health" != "Green" ]; then
   echo "Target environment is not healthy"
