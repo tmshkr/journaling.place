91f84d00dee995461cc6b0db078c0c7238a31bfb
Tim Shaker <tim@tmshkr.com>
2024-01-20 15:59:50 -0800

    PasswordInput wip

--- apps/web/src/components/modals/PasswordInput.tsx
+++ apps/web/src/components/modals/PasswordInput.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Dialog } from "@headlessui/react";
-import { useAppDispatch, useAppSelector } from "src/store";
+import { useAppSelector } from "src/store";
 import { selectUser } from "src/store/user";
-import { selectModal } from "src/store/modal";
 import { createKey } from "src/lib/crypto";
 import { useForm } from "react-hook-form";
 
@@ -13,26 +11,58 @@ const buttonClasses =
 const inputClasses =
   "block w-full rounded-md border-0 py-1.5 my-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6";
 
+enum PasswordInputState {
+  CreatePassword,
+  ConfirmPassword,
+  EnterPassword,
+}
+
 export function PasswordInput() {
   const user = useAppSelector(selectUser);
+  const [passwordInputState, setPasswordInputState] = useState(
+    user.salt
+      ? PasswordInputState.EnterPassword
+      : PasswordInputState.CreatePassword
+  );
 
-  const hasPassword = !!user.salt;
-
-  const { register, handleSubmit, formState, setError, setFocus } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState,
+    setError,
+    setFocus,
+    setValue,
+    reset,
+  } = useForm();
   const { errors }: { errors: any } = formState;
+  const hasErrors = Object.keys(errors).length > 0;
 
   const onSubmit = async (values) => {
     console.log("values", values);
     const { password, confirm_password } = values;
-    if (!hasPassword && password !== confirm_password) {
-      console.log("passwords do not match");
-      setError("confirm_password", {
-        message: "Passwords do not match.",
-      });
-      setFocus("confirm_password");
-      return;
+
+    switch (passwordInputState) {
+      case PasswordInputState.CreatePassword:
+        setPasswordInputState(PasswordInputState.ConfirmPassword);
+        setValue("confirm_password", "");
+        setFocus("confirm_password");
+        return;
+      case PasswordInputState.ConfirmPassword:
+        if (password !== confirm_password) {
+          console.log("passwords do not match");
+          setError("confirm_password", {
+            message: "Passwords do not match.",
+          });
+          setFocus("confirm_password");
+          return;
+        }
+        break;
+      case PasswordInputState.EnterPassword:
+        await createKey(password);
+        break;
+      default:
+        throw new Error("Invalid password input state.");
     }
-    await createKey(password);
   };
 
   useEffect(() => {
@@ -43,56 +73,131 @@ export function PasswordInput() {
     <form onSubmit={handleSubmit(onSubmit)} className="text-center">
       <Dialog.Title
         as="label"
-        htmlFor="password"
+        htmlFor={
+          passwordInputState === PasswordInputState.ConfirmPassword
+            ? "confirm_password"
+            : "password"
+        }
         className="block text-md font-medium leading-6 text-gray-900"
       >
-        Journal Password
+        {passwordInputState === PasswordInputState.EnterPassword
+          ? "Journal Password"
+          : "Create Password"}
       </Dialog.Title>
       <div className="mt-2">
-        <input
-          id="password"
-          type="password"
-          {...register("password", {
-            required: "You must provide your password.",
-          })}
-          className={inputClasses}
-        />
+        {passwordInputState === PasswordInputState.ConfirmPassword ? (
+          <input
+            id="confirm_password"
+            aria-label="confirm password"
+            type="password"
+            className={inputClasses}
+            disabled={hasErrors}
+            {...register("confirm_password", {
+              required: "You must confirm your password.",
+            })}
+          />
+        ) : (
+          <input
+            id="password"
+            aria-label="enter password"
+            type="password"
+            className={inputClasses}
+            {...register("password", {
+              required: "You must provide your password.",
+            })}
+          />
+        )}
       </div>
-      {!hasPassword && (
-        <>
-          <label
-            htmlFor="confirm_password"
-            className="block text-md font-medium leading-6 text-gray-900"
-          >
-            Confirm Password
-          </label>
-          <div className="mt-2">
-            <input
-              id="confirm_password"
-              type="password"
-              {...register("confirm_password", {
-                required: "You must confirm your password.",
-              })}
-              className={inputClasses}
-            />
-          </div>
-        </>
-      )}
-      {hasPassword ? (
+      <InputDescription {...{ errors, hasErrors, passwordInputState }} />
+      <SubmitButton
+        {...{
+          errors,
+          passwordInputState,
+          hasErrors,
+          reset,
+          setPasswordInputState,
+        }}
+      />
+    </form>
+  );
+}
+
+function InputDescription({ passwordInputState, errors, hasErrors }) {
+  console.log(errors);
+  if (hasErrors) {
+    return Object.keys(errors).map((key) => (
+      <Dialog.Description key={key} className="text-sm text-red-500 my-2">
+        {errors[key].message}
+      </Dialog.Description>
+    ));
+  }
+
+  switch (passwordInputState) {
+    case PasswordInputState.CreatePassword:
+      return (
         <Dialog.Description className="text-sm text-gray-500 my-2">
-          Please enter your password to access your journal.
-        </Dialog.Description>
-      ) : (
-        <Dialog.Description className="text-sm text-gray-500 my-2">
+          Please create a password to encrypt your journal.
+          <br />
           Use a strong password and store it in a safe place.
           <br />
           If you lose your password, your data cannot be recovered.
         </Dialog.Description>
-      )}
+      );
+    case PasswordInputState.ConfirmPassword:
+      return (
+        <Dialog.Description className="text-sm text-gray-500 my-2">
+          Please confirm your password.
+        </Dialog.Description>
+      );
+    case PasswordInputState.EnterPassword:
+      return (
+        <Dialog.Description className="text-sm text-gray-500 my-2">
+          Please enter your password.
+        </Dialog.Description>
+      );
+    default:
+      throw new Error("Invalid password input state.");
+  }
+}
 
-      <button id="password_submit" type="submit" className={buttonClasses}>
-        Submit
-      </button>
-    </form>
-  );
+function SubmitButton({
+  passwordInputState,
+  errors,
+  hasErrors,
+  reset,
+  setPasswordInputState,
+}) {
+  switch (passwordInputState) {
+    case PasswordInputState.CreatePassword:
+      return (
+        <button id="password_submit" type="submit" className={buttonClasses}>
+          Next
+        </button>
+      );
+    case PasswordInputState.ConfirmPassword:
+      return hasErrors ? (
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setPasswordInputState(PasswordInputState.CreatePassword);
+            reset();
+          }}
+          className={buttonClasses}
+        >
+          Try Again
+        </button>
+      ) : (
+        <button id="password_submit" type="submit" className={buttonClasses}>
+          Submit
+        </button>
+      );
+    case PasswordInputState.EnterPassword:
+      return (
+        <button id="password_submit" type="submit" className={buttonClasses}>
+          Submit
+        </button>
+      );
+    default:
+      throw new Error("Invalid password input state.");
+  }
 }
