4606713570bc7ef2238bc86f0494ae05b604566b
Tim Shaker <tim@tmshkr.com>
2024-01-25 10:14:02 -0800

    add already-unsubcribed page

--- apps/web/src/pages/api/email/unsubscribe.ts
+++ apps/web/src/pages/api/email/unsubscribe.ts
@@ -1,4 +1,3 @@
-import { NotificationTopic } from "@prisma/client";
 import { mongoClient } from "src/lib/mongo";
 import { decode } from "next-auth/jwt";
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -23,8 +22,7 @@ async function handleToken(token: string) {
     });
     if (!decodedToken) throw new Error("Token is not valid");
     z.object({
-      email: z.string(),
-      topic: z.enum(Object.keys(NotificationTopic) as any),
+      email: z.string().email(),
     }).parse(decodedToken);
 
     return decodedToken;
@@ -35,48 +33,79 @@ async function handleToken(token: string) {
 
 const topicMeta = {
   prompt_of_the_day: {
+    key: "prompt_of_the_day",
     title: "Prompt of the Day",
   },
 };
 
 async function handleGet(req: NextApiRequest, res: NextApiResponse) {
-  const { email, topic } = await handleToken(req.query.token as string);
-  const unsubscribePage = pug.compileFile(
-    path.resolve(process.cwd(), "src/pages/api/email/unsubscribe.pug")
-  );
-  return res.send(
-    unsubscribePage({
-      url: req.url,
-      email,
-      title: topicMeta[topic as string].title,
-    })
-  );
+  const topic = topicMeta[req.query.topic as string];
+  if (!topic) throw createError.BadRequest("Invalid topic");
+
+  const { email } = await handleToken(req.query.token as string);
+
+  const user = await mongoClient
+    .db()
+    .collection("User")
+    .findOne({ email }, { projection: { emailNotifications: 1 } });
+
+  if (!user) throw createError.NotFound("User not found");
+
+  if (user.emailNotifications.includes(topic.key)) {
+    return res.send(
+      pug.renderFile(
+        path.resolve(process.cwd(), "src/pages/api/email/unsubscribe.pug"),
+        {
+          email,
+          title: topic.title,
+        }
+      )
+    );
+  } else {
+    return res.send(
+      pug.renderFile(
+        path.resolve(
+          process.cwd(),
+          "src/pages/api/email/already-unsubscribed.pug"
+        ),
+        {
+          email,
+          title: topic.title,
+        }
+      )
+    );
+  }
 }
 
 async function handlePost(req: NextApiRequest, res: NextApiResponse) {
-  const { email, topic } = await handleToken(req.query.token as string);
-  const { title } = topicMeta[topic as string];
-
-  const unsubscribedPage = pug.compileFile(
-    path.resolve(process.cwd(), "src/pages/api/email/unsubscribed.pug")
-  );
-
-  const resubscribedPage = pug.compileFile(
-    path.resolve(process.cwd(), "src/pages/api/email/resubscribed.pug")
-  );
+  const topic = topicMeta[req.query.topic as string];
+  if (!topic) throw createError.BadRequest("Invalid topic");
+  const { email } = await handleToken(req.query.token as string);
 
   if (req.body.resubscribe) {
     await mongoClient
       .db()
       .collection("User")
-      .updateOne({ email }, { $addToSet: { emailNotifications: topic } });
-    return res.send(resubscribedPage({ email, title }));
+      .updateOne({ email }, { $addToSet: { emailNotifications: topic.key } });
+
+    return res.send(
+      pug.renderFile(
+        path.resolve(process.cwd(), "src/pages/api/email/resubscribed.pug"),
+        { email, title: topic.title }
+      )
+    );
   } else {
     await mongoClient
       .db()
       .collection("User")
-      .updateOne({ email }, { $pull: { emailNotifications: topic } });
-    return res.send(unsubscribedPage({ email, title }));
+      .updateOne({ email }, { $pull: { emailNotifications: topic.key } });
+
+    return res.send(
+      pug.renderFile(
+        path.resolve(process.cwd(), "src/pages/api/email/unsubscribed.pug"),
+        { email, title: topic.title }
+      )
+    );
   }
 }
 
