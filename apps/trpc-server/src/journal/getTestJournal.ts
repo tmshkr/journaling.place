aaace41b07b4c97cdda5e7c855a0b4ebff72e028
Tim Shaker <tim@tmshkr.com>
2024-01-26 11:48:54 -0800

    getTestJournal returns an active or trashed journal

--- apps/trpc-server/src/journal/getTestJournal.ts
+++ apps/trpc-server/src/journal/getTestJournal.ts
@@ -1,13 +1,21 @@
 import { authorizedProcedure } from "..";
 
-// Returns an active journal to test the encryption key on the client
+// Returns a journal to test the encryption key on the client
 export const getTestJournal = authorizedProcedure.query(async ({ ctx }) => {
   const { user, prisma } = ctx;
 
   const [journal] = await prisma.journal.findMany({
     where: {
-      authorId: user.id,
-      status: "ACTIVE",
+      OR: [
+        {
+          authorId: user.id,
+          status: "ACTIVE",
+        },
+        {
+          authorId: user.id,
+          status: "TRASHED",
+        },
+      ],
     },
     take: 1,
   });
