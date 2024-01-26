e4afcf7af15727fa2d767841ce31cd197cace8f0
Tim Shaker <tim@tmshkr.com>
2024-01-26 11:00:44 -0800

    await indexedDB clear

--- apps/web/src/services/auth.ts
+++ apps/web/src/services/auth.ts
@@ -2,9 +2,9 @@ import { signOut } from "next-auth/react";
 import { cryptoStore, journalStore } from "src/services/localForage";
 import { clearKey } from "src/services/crypto";
 
-export function logout() {
+export async function logout() {
+  await cryptoStore.clear();
+  await journalStore.clear();
   clearKey();
   signOut();
-  cryptoStore.clear();
-  journalStore.clear();
 }
