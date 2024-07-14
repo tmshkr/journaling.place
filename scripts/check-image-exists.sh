b174b996dd96eafe152935f7c1c996608707a502
Tim Shaker <tim@tmshkr.com>
2024-07-13 17:32:58 -0700

    update check-image-exists.sh

--- scripts/check-image-exists.sh
+++ scripts/check-image-exists.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -e
 
-
 if output=$(aws ecr describe-images --repository-name=journaling.place --image-ids imageTag=$1 2>&1); then
   echo "true"
+  if [ -z "$GITHUB_OUTPUT" ]; then
+    echo IMAGE_EXISTS=true >>$GITHUB_OUTPUT
+  fi
 else
-    if [[ $output == *"ImageNotFoundException"* ]]; then
-        echo "false"
-    else
-        echo "Command failed with error: $output"
-        exit 1
+  if [[ $output == *"ImageNotFoundException"* ]]; then
+    echo "false"
+    if [ -z "$GITHUB_OUTPUT" ]; then
+      echo IMAGE_EXISTS=false >>$GITHUB_OUTPUT
     fi
+  else
+    echo "Command failed with error: $output"
+    exit 1
+  fi
 fi
