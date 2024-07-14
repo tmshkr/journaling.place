769c09c85643cc3bd6e59f96ebef4d5863cbd0f0
Tim Shaker <tim@tmshkr.com>
2024-07-13 18:05:42 -0700

    check if image exists in 15-compose-file.sh

--- .platform/hooks/prebuild/15-compose-file.sh
+++ .platform/hooks/prebuild/15-compose-file.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
 source .env
-
-if [ "$PREP" == "true" ]; then
-  cp docker-compose.prep.yml docker-compose.yml
+# check if the image exists in ECR
+if output=$(./scripts/check-image-exists.sh $GITHUB_SHA); then
+  if [ "$output" == "true" ]; then
+    cp docker-compose.run.yml docker-compose.yml
+  else
+    cp docker-compose.prep.yml docker-compose.yml
+  fi
 else
-  cp docker-compose.run.yml docker-compose.yml
+  echo "There was an error checking the image: $output"
+  exit 1
 fi
