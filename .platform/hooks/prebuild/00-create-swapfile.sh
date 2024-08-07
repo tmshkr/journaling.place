a87605781eb64cbd5affc558ef269fbf67059aaa
Tim Shaker <tim@tmshkr.com>
2024-08-07 12:16:40 -0700

    create swapfile

---
+++ .platform/hooks/prebuild/00-create-swapfile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o xtrace
+set -e
+
+if grep -E 'SwapTotal:\s+0+\s+kB' /proc/meminfo; then
+    echo "Positively identified no swap space, creating some."
+    dd if=/dev/zero of=/var/swapfile bs=1M count=512
+    /sbin/mkswap /var/swapfile
+    chmod 0600 /var/swapfile
+    /sbin/swapon /var/swapfile
+else
+    echo "Did not confirm zero swap space, doing nothing."
+fi
+
+# https://stackoverflow.com/a/36119307/8229853
