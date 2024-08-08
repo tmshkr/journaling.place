d03924fcde120d305b4c6251ddf39d669684550e
Tim Shaker <tim@tmshkr.com>
2024-08-08 16:39:38 -0700

    handle-changed-files.sh

--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -e
 
-echo $GH_EVENT
-echo "Any changed: $ANY_CHANGED"
-for file in ${ALL_CHANGED_FILES}; do
-    echo "$file was changed"
-done
+# echo $GITHUB_EVENT_NAME
+
+commits=$(git rev-list -n 2 HEAD)
+read -r curr prev <<<$commits
+
+changed_files=$(git diff --name-only $prev $curr)
+if [ -z "$changed_files" ]; then
+    echo "No files have changed between $prev and $curr."
+    echo "Attempting to tag the existing image with the new tag."
+    CURRENT_TAG=$prev NEW_TAG=$curr scripts/tag-image.js
+else
+    echo "Changed files between $prev and $curr:"
+    for file in $changed_files; do
+        echo "$file"
+    done
+fi
