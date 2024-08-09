714e1ba27b5528608e5b4b6a976a524890b65999
Tim Shaker <tim@tmshkr.com>
2024-08-08 18:24:18 -0700

    add pr condition

--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -1,15 +1,17 @@
-#!/bin/bash -e
-echo $GH_EVENT
-if [[$GITHUB_EVENT_NAME == "pull_request"]]; then
-    echo "TODO: Implement logic for pull requests."
-    exit 0
+#!/bin/bash
+
+set -e
+
+if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+    base_sha=$(echo $GH_EVENT | jq -r '.pull_request.base.head.sha')
+    head_sha=$(echo $GH_EVENT | jq -r '.pull_request.head.sha')
 else
     commits=$(git rev-list -n 2 HEAD)
-    read -r curr prev <<<$commits
+    read -r head_sha base_sha <<<$commits
 fi
 
 if [ "$ANY_CHANGED" == "false" ]; then
-    echo "No files have changed between $prev and $curr."
+    echo "No files have changed between $base_sha and $head_sha."
     echo "Attempting to tag the existing image with the new tag."
-    CURRENT_TAG=$prev NEW_TAG=$curr scripts/tag-image.js
+    CURRENT_TAG=$base_sha NEW_TAG=$head_sha scripts/tag-image.js
 fi
