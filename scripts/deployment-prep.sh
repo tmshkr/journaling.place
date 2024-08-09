874aee8f426033d7a57a4d64720bf8a61fb1702f
Tim Shaker <tim@tmshkr.com>
2024-08-09 16:10:37 -0700

    refactor scripts

--- scripts/deployment-prep.sh
+++ scripts/deployment-prep.sh
@@ -10,4 +10,12 @@ echo "VERSION_LABEL=$VERSION_LABEL" >>$GITHUB_OUTPUT
 
 scripts/handle-changed-files.sh
 scripts/check-image-exists.sh $TAG
-scripts/generate-deployment-package.sh
+
+echo APP_VERSION=$APP_VERSION >>.env
+echo GITHUB_REF_NAME=$GITHUB_REF_NAME >>.env
+echo GITHUB_SHA=$GITHUB_SHA >>.env
+echo TAG=$TAG >>.env
+echo VERSION_LABEL=$VERSION_LABEL >>.env
+
+source ./scripts/sub-option-settings.sh
+zip -r bundle.zip . -x '*.git*'
--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -1,26 +1,66 @@
 #!/bin/bash
 
 set -eo pipefail
-echo "GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME"
-ref_name="${GITHUB_REF_NAME//\//_}"
 
-if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$ref_name 2>&1); then
-    tags=$(echo $images | jq -r '.imageDetails[0].imageTags[]')
+find-before-sha() {
+    echo "Finding build_sha of image tagged with $1."
+    if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$1 2>&1); then
+        tags=$(echo $images | jq -r '.imageDetails[0].imageTags[]')
 
-    for tag in $tags; do
-        if [[ $tag == "$ref_name-"* ]]; then
-            before="${tag#"$ref_name-"}"
-            break
-        fi
-    done
-else
-    if [[ "$images" == *"ImageNotFoundException"* ]]; then
-        echo "No images found with tag $ref_name."
+        for tag in $tags; do
+            if [[ $tag == "build_sha."* ]]; then
+                before="${tag#"build_sha."}"
+                break
+            fi
+        done
     else
-        echo "There was an error while trying to describe images."
-        echo $images
+        if [[ "$images" == *"ImageNotFoundException"* ]]; then
+            echo "No images found with tag $1."
+        else
+            echo "There was an error while trying to describe images."
+            echo $images
+            exit 1
+        fi
+    fi
+}
+
+compare-shas() {
+    if [[ -z "$1" || -z "$2" ]]; then
+        echo "No SHAs provided."
         exit 1
     fi
+
+    if [[ "$1" == "$2" ]]; then
+        echo "The SHAs are the same."
+        exit 0
+    fi
+
+    echo "Comparing $1 with $2."
+    changes=$(gh api \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        /repos/$GITHUB_REPOSITORY/compare/$1...$2)
+    changed_files=$(echo $changes | jq -r '.files[].filename')
+
+    if [[ -z "$changed_files" ]]; then
+        echo "No files have changed."
+        echo "Attempting to tag the existing image with the new tag."
+        CURRENT_TAG=$1 NEW_TAG=$2 scripts/tag-image.js
+    else
+        echo "Files that have changed:"
+        for file in $changed_files; do
+            echo "$file"
+        done
+    fi
+}
+
+echo "GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME"
+
+after=$GITHUB_SHA
+find-before-sha "${GITHUB_REF_NAME//\//_}"
+
+if [[ -z "$before" ]]; then
+    find-before-sha "latest"
 fi
 
 if [[ -z "$before" ]]; then
@@ -28,20 +68,4 @@ if [[ -z "$before" ]]; then
     exit 0
 fi
 
-echo "Comparing $before with $GITHUB_SHA."
-changes=$(gh api \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    /repos/$GITHUB_REPOSITORY/compare/$before...$GITHUB_SHA)
-changed_files=$(echo $changes | jq -r '.files[].filename')
-
-if [[ -z "$changed_files" ]]; then
-    echo "No files have changed."
-    echo "Attempting to tag the existing image with the new tag."
-    CURRENT_TAG=$before NEW_TAG=$GITHUB_SHA scripts/tag-image.js
-else
-    echo "Files that have changed:"
-    for file in $changed_files; do
-        echo "$file"
-    done
-fi
+compare-shas $before $after
