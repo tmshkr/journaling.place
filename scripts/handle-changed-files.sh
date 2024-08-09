daae36e27534e1b5fa7b81bc69097682f785d1ae
Tim Shaker <tim@tmshkr.com>
2024-08-09 12:18:31 -0700

    handle PR GITHUB_REF_NAME

--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -4,21 +4,22 @@ set -eo pipefail
 
 before=$(echo $GH_EVENT | jq -r '.before')
 after=$(echo $GH_EVENT | jq -r '.after')
+ref_name="${GITHUB_REF_NAME//\//_}"
 
 if [[ "$before" == "null" || "$after" == "null" ]]; then
-    if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$GITHUB_REF_NAME 2>&1); then
+    if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$ref_name 2>&1); then
         tags=$(echo $images | jq -r '.imageDetails[0].imageTags[]')
 
         for tag in $tags; do
-            if [[ $tag == "$GITHUB_REF_NAME-"* ]]; then
-                before="${tag#"$GITHUB_REF_NAME-"}"
+            if [[ $tag == "$ref_name-"* ]]; then
+                before="${tag#"$ref_name-"}"
                 after=$GITHUB_SHA
                 break
             fi
         done
     else
         if [[ "$images" == *"ImageNotFoundException"* ]]; then
-            echo "No images found with tag $GITHUB_REF_NAME."
+            echo "No images found with tag $ref_name."
             exit 0
         else
             echo "There was an error while trying to describe images."
