39eff23e216583d309e3a84fc00918e123a73214
Tim Shaker <tim@tmshkr.com>
2024-08-09 11:55:09 -0700

    log tags

--- scripts/tag-image.js
+++ scripts/tag-image.js
@@ -5,6 +5,8 @@ if (!CURRENT_TAG || !NEW_TAG) {
   throw new Error("CURRENT_TAG and NEW_TAG must be set");
 }
 
+console.log("Tagging image", { CURRENT_TAG, NEW_TAG });
+
 const { images, failures } = JSON.parse(
   execSync(
     `aws ecr batch-get-image --repository-name journaling.place --image-ids imageTag=${CURRENT_TAG} --output json`
