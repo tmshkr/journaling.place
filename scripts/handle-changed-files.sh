c9cfd0ebee0cab96be4b855b4105bcc51452c8a2
Tim Shaker <tim@tmshkr.com>
2024-08-09 16:16:20 -0700

    refactor

--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-find-before-sha() {
+find_build_sha() {
     echo "Finding build_sha of image tagged with $1."
     if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$1 2>&1); then
         tags=$(echo $images | jq -r '.imageDetails[0].imageTags[]')
@@ -13,6 +13,9 @@ find-before-sha() {
                 break
             fi
         done
+        if [[ -z "$before" ]]; then
+            echo "No build_sha found for tag $1."
+        fi
     else
         if [[ "$images" == *"ImageNotFoundException"* ]]; then
             echo "No images found with tag $1."
@@ -24,15 +27,15 @@ find-before-sha() {
     fi
 }
 
-compare-shas() {
+compare_shas() {
     if [[ -z "$1" || -z "$2" ]]; then
         echo "No SHAs provided."
-        exit 1
+        return 1
     fi
 
     if [[ "$1" == "$2" ]]; then
         echo "The SHAs are the same."
-        exit 0
+        return 0
     fi
 
     echo "Comparing $1 with $2."
@@ -57,10 +60,10 @@ compare-shas() {
 echo "GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME"
 
 after=$GITHUB_SHA
-find-before-sha "${GITHUB_REF_NAME//\//_}"
+find_build_sha "${GITHUB_REF_NAME//\//_}"
 
 if [[ -z "$before" ]]; then
-    find-before-sha "latest"
+    find_build_sha "latest"
 fi
 
 if [[ -z "$before" ]]; then
@@ -68,4 +71,4 @@ if [[ -z "$before" ]]; then
     exit 0
 fi
 
-compare-shas $before $after
+compare_shas $before $after
