27278b917e2086f13b7bfd80ac4fe9b9fced3037
Tim Shaker <tim@tmshkr.com>
2024-08-08 18:09:24 -0700

    handle-change-files wip

--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -e
 
-# echo $GITHUB_EVENT_NAME
+if [[$GITHUB_EVENT_NAME == "pull_request"]]; then
+    echo "TODO: Implement logic for pull requests."
+    exit 0
+else
+    commits=$(git rev-list -n 2 HEAD)
+    read -r curr prev <<<$commits
+fi
 
-commits=$(git rev-list -n 2 HEAD)
-read -r curr prev <<<$commits
-
-changed_files=$(git diff --name-only $prev $curr)
-if [ -z "$changed_files" ]; then
+if [ "$ANY_CHANGED" == "false" ]; then
     echo "No files have changed between $prev and $curr."
     echo "Attempting to tag the existing image with the new tag."
     CURRENT_TAG=$prev NEW_TAG=$curr scripts/tag-image.js
-else
-    echo "Changed files between $prev and $curr:"
-    for file in $changed_files; do
-        echo "$file"
-    done
 fi
