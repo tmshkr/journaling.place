9afe77800e2a78969552eeb74d40b0f1fb1191fb
Tim Shaker <tim@tmshkr.com>
2024-08-09 12:34:03 -0700

    use GITHUB_SHA instead of GH_EVENT

--- scripts/handle-changed-files.sh
+++ scripts/handle-changed-files.sh
@@ -2,45 +2,45 @@
 
 set -eo pipefail
 
-before=$(echo $GH_EVENT | jq -r '.before')
-after=$(echo $GH_EVENT | jq -r '.after')
 ref_name="${GITHUB_REF_NAME//\//_}"
 
-if [[ "$before" == "null" || "$after" == "null" ]]; then
-    if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$ref_name 2>&1); then
-        tags=$(echo $images | jq -r '.imageDetails[0].imageTags[]')
+if images=$(aws ecr describe-images --repository-name "${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$ref_name 2>&1); then
+    tags=$(echo $images | jq -r '.imageDetails[0].imageTags[]')
 
-        for tag in $tags; do
-            if [[ $tag == "$ref_name-"* ]]; then
-                before="${tag#"$ref_name-"}"
-                after=$GITHUB_SHA
-                break
-            fi
-        done
-    else
-        if [[ "$images" == *"ImageNotFoundException"* ]]; then
-            echo "No images found with tag $ref_name."
-            exit 0
-        else
-            echo "There was an error while trying to describe images."
-            echo $images
-            exit 1
+    for tag in $tags; do
+        if [[ $tag == "$ref_name-"* ]]; then
+            before="${tag#"$ref_name-"}"
+            break
         fi
+    done
+else
+    if [[ "$images" == *"ImageNotFoundException"* ]]; then
+        echo "No images found with tag $ref_name."
+        exit 0
+    else
+        echo "There was an error while trying to describe images."
+        echo $images
+        exit 1
     fi
+fi
+
+if [[ -z "$before" ]]; then
+    echo "No previous commit to compare."
+    exit 0
 fi
 
 changes=$(gh api \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    /repos/$GITHUB_REPOSITORY/compare/$before...$after)
+    /repos/$GITHUB_REPOSITORY/compare/$before...$GITHUB_SHA)
 changed_files=$(echo $changes | jq -r '.files[].filename')
 
 if [[ -z "$changed_files" ]]; then
-    echo "No files have changed between $before and $after."
+    echo "No files have changed between $before and $GITHUB_SHA."
     echo "Attempting to tag the existing image with the new tag."
-    CURRENT_TAG=$before NEW_TAG=$after scripts/tag-image.js
+    CURRENT_TAG=$before NEW_TAG=$GITHUB_SHA scripts/tag-image.js
 else
-    echo "Files that have changed between $before and $after:"
+    echo "Files that have changed between $before and $GITHUB_SHA:"
     for file in $changed_files; do
         echo "$file"
     done
