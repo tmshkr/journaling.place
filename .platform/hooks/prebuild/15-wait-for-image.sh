ebafdf628b3027d83ef4110429a840080ce57c69
Tim Shaker <tim@tmshkr.com>
2024-08-10 15:54:12 -0700

    refactor wait-for-image

--- .platform/hooks/prebuild/15-wait-for-image.sh
+++ .platform/hooks/prebuild/15-wait-for-image.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -e
 
 source .env
+check_image_exists() {
+  if out=$(aws ecr describe-images --repository-name="${GITHUB_REPOSITORY#*/}" --image-ids imageTag=$TAG 2>&1); then
+    echo "true"
+  else
+    if [[ $out == *"ImageNotFoundException"* ]]; then
+      echo "false"
+    else
+      echo $out
+      return 1
+    fi
+  fi
+}
+
 # wait for the image to be ready
-while output=$(./scripts/check-image-exists.sh $TAG); do
-  if [ "$output" == "true" ]; then
-    echo "Image with tag $TAG is ready. Proceeding..."
+while out=$(check_image_exists); do
+  if [[ $out == "true" ]]; then
+    echo "Image with tag [$TAG] is ready. Proceeding..."
     exit 0
   fi
   echo "Image not ready yet. Retrying in 10 seconds..."
   sleep 10
 done
 
-echo "There was an error checking the image."
-echo $output
-exit 1
+echo "There was an error checking the image:"
+echo $out
