105ff0037e4ea49d4525f5df0772c4873505214d
Tim Shaker <tim@tmshkr.com>
2024-08-10 16:53:47 -0700

    update deploy

--- scripts/deployment-prep.sh
+++ scripts/deployment-prep.sh
@@ -8,19 +8,17 @@ export VERSION_LABEL="$LABEL_REF_NAME.$GITHUB_SHA"
 echo "LABEL_REF_NAME=$LABEL_REF_NAME" >>$GITHUB_OUTPUT
 echo "VERSION_LABEL=$VERSION_LABEL" >>$GITHUB_OUTPUT
 
-scripts/handle-changed-files.sh
+scripts/handle-turbo-sha.mjs
 
-IMAGE_EXISTS=$(scripts/check-image-exists.sh $TAG)
-echo "IMAGE_EXISTS=$IMAGE_EXISTS"
-echo "IMAGE_EXISTS=$IMAGE_EXISTS" >>$GITHUB_OUTPUT
+alb_stack=$(aws cloudformation describe-stacks --stack-name ALBStack)
+export SHARED_LOAD_BALANCER_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="SharedLoadBalancerArn") | .OutputValue')
+if [ -z "$SHARED_LOAD_BALANCER_ARN" ]; then
+    echo "SHARED_LOAD_BALANCER_ARN is not available"
+    exit 1
+fi
 
-echo APP_VERSION=$APP_VERSION >>.env
-echo GITHUB_REF_NAME=$GITHUB_REF_NAME >>.env
-echo GITHUB_SHA=$GITHUB_SHA >>.env
-echo TAG=$TAG >>.env
-echo VERSION_LABEL=$VERSION_LABEL >>.env
-
-scripts/sub-option-settings.sh
+echo $(envsubst <deploy-meta.json) >deploy-meta.json
+echo $(envsubst <option-settings.json) >option-settings.json
 
 echo "Creating bundle.zip"
 zip -r bundle.zip . -x '*.git*'
