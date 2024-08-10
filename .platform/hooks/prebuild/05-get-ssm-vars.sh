f362571be616f183357aa4079079e9fac0c88d39
Tim Shaker <tim@tmshkr.com>
2024-08-10 16:52:15 -0700

    consolidate env var hooks

--- .platform/hooks/prebuild/05-get-ssm-vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-source .env
-
-# Retrieve parameters from AWS SSM
-aws ssm get-parameters-by-path --path "/journaling.place/$STAGE/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text | while read -r name value; do
-  key=$(echo $name | sed "s/\/journaling.place\/$STAGE\///")
-  echo "$key=$value" >>.env
-done
---
+++ .platform/hooks/prebuild/05-handle-env-vars.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eo pipefail
+
+get-config() {
+    /opt/elasticbeanstalk/bin/get-config "$@"
+}
+
+environment_name=$(get-config container | jq -r '.environment_name')
+echo "EB_ENVIRONMENT_NAME=$environment_name" >>.env
+
+get-config environment | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
+    echo $line >>.env
+done
+
+CNAME=$(aws elasticbeanstalk describe-environments --environment-names $environment_name --no-include-deleted | jq -r '.Environments[0].CNAME')
+echo "CNAME=$CNAME" >>.env
+if [[ "$CNAME" == *production* ]]; then
+    echo "STAGE=production" >>.env
+elif [[ "$CNAME" == *staging* ]]; then
+    echo "STAGE=staging" >>.env
+else
+    echo "There was an error determining the environment stage"
+    exit 1
+fi
+
+echo deploy-meta.json | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
+    echo $line >>.env
+done
+
+source .env
+# Retrieve parameters from AWS SSM
+repo="${GITHUB_REPOSITORY#*/}"
+aws ssm get-parameters-by-path --path "/$repo/$STAGE/" \
+    --recursive --with-decryption --query "Parameters[*].[Name,Value]" \
+    --output text | while read -r name value; do
+    key=$(echo $name | sed "s/\/$repo\/$STAGE\///")
+    echo "$key=$value" >>.env
+done
