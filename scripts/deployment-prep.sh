83d0259acb5e8b0c80dafca85bdc48c32cd5e0fe
Tim Shaker <tim@tmshkr.com>
2024-08-12 17:25:17 -0700

    update deployment prep

--- scripts/deployment-prep.sh
+++ scripts/deployment-prep.sh
@@ -2,20 +2,28 @@
 
 set -eo pipefail
 
+detect_empty_values() {
+    if jq '.[] | test("^$")' $1 | grep true; then
+        echo "Empty value detected in $1"
+        jq 'to_entries | map(select(.value == ""))' $1
+        exit 1
+    fi
+}
+
 export VERSION_LABEL="${GITHUB_REF_NAME//\//_}.$GITHUB_SHA"
 echo "VERSION_LABEL=$VERSION_LABEL" >>$GITHUB_OUTPUT
 
-scripts/handle-turbo-sha.mjs
+scripts/turbo-sha.mjs
+source $GITHUB_ENV
 
 alb_stack=$(aws cloudformation describe-stacks --stack-name ALBStack)
 export SHARED_LOAD_BALANCER_ARN=$(echo $alb_stack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="SharedLoadBalancerArn") | .OutputValue')
-if [ -z "$SHARED_LOAD_BALANCER_ARN" ]; then
-    echo "SHARED_LOAD_BALANCER_ARN is not available"
-    exit 1
-fi
 
 echo $(envsubst <deploy-vars.json) >deploy-vars.json
 echo $(envsubst <option-settings.json) >option-settings.json
+
+detect_empty_values deploy-vars.json
+detect_empty_values option-settings.json
 
 echo "Creating bundle.zip"
 zip -r bundle.zip . -x '*.git*'
