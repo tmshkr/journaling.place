9fd7539a93c1a2c3e5baf77d4c95dddeb0901785
Tim Shaker <tim@tmshkr.com>
2024-08-14 11:06:57 -0700

    update export-build-output.sh

--- scripts/export-build-output.sh
+++ scripts/export-build-output.sh
@@ -10,31 +10,43 @@ print_heading() {
     echo
 }
 
-print_heading "=" "Exporting build output to $(pwd)/build_output"
+export_build_output() {
+    print_heading "=" "Exporting build output to $(pwd)/build_output"
 
-mkdir -p build_output/node_modules/.bin
-mkdir -p build_output/apps/agenda-worker/
-mkdir -p build_output/apps/trpc-server/
-mkdir -p build_output/apps/web/.next/
+    mkdir -p build_output/node_modules/.bin
+    mkdir -p build_output/apps/agenda-worker/
+    mkdir -p build_output/apps/trpc-server/
+    mkdir -p build_output/apps/web/.next/
 
-print_heading "*" "Copying package.json"
+    print_heading "*" "Copying package.json"
 
-cp -vR package*.json build_output/
-cp -vR apps/agenda-worker/package.json build_output/apps/agenda-worker/
-cp -vR apps/trpc-server/package.json build_output/apps/trpc-server/
-cp -vR apps/web/package.json build_output/apps/web/
+    cp -vR package*.json build_output/
+    cp -vR apps/agenda-worker/package.json build_output/apps/agenda-worker/
+    cp -vR apps/trpc-server/package.json build_output/apps/trpc-server/
+    cp -vR apps/web/package.json build_output/apps/web/
 
-print_heading "*" "Copying dist"
+    print_heading "*" "Copying dist"
 
-cp -vR apps/agenda-worker/dist build_output/apps/agenda-worker/
-cp -vR apps/trpc-server/dist build_output/apps/trpc-server/
-cp -vR apps/web/.next build_output/apps/web/
+    cp -vR apps/agenda-worker/dist build_output/apps/agenda-worker/
+    cp -vR apps/trpc-server/dist build_output/apps/trpc-server/
+    cp -vR apps/web/.next build_output/apps/web/
 
-print_heading "*" "Copying node_modules"
+    print_heading "*" "Copying node_modules"
 
-cd node_modules
-cp -vR .bin/turbo .bin/prisma ../build_output/node_modules/.bin/
-cp -vR .prisma *prisma turbo* ../build_output/node_modules/
+    cd node_modules
+    cp -vR .bin/turbo .bin/prisma ../build_output/node_modules/.bin/
+    cp -vR .prisma *prisma turbo* ../build_output/node_modules/
+    cd ..
 
-cd ..
-print_heading "=" "$(du -sh build_output | expand)"
+    print_heading "*" "Copying other files"
+
+    cp -vR data build_output/
+    cp -vR prisma build_output/
+    cp -vR scripts build_output/
+    cp -vR turbo.json build_output/
+
+    print_heading "=" "$(du -sh build_output | expand)"
+}
+
+mkdir -p build_output
+export_build_output >build_output/export-build-output.log 2>&1
