1650c53befaf283cd43ade8fc27510b276f9d9ba
Tim Shaker <tim@tmshkr.com>
2024-12-28 14:12:40 -0800

    update swapfile script

--- .platform/hooks/prebuild/00-create-swapfile.sh
+++ .platform/hooks/prebuild/00-create-swapfile.sh
@@ -5,10 +5,10 @@ set -e
 
 if grep -E 'SwapTotal:\s+0+\s+kB' /proc/meminfo; then
     echo "Positively identified no swap space, creating some."
-    dd if=/dev/zero of=/var/swapfile bs=1M count=512
-    /sbin/mkswap /var/swapfile
-    chmod 0600 /var/swapfile
-    /sbin/swapon /var/swapfile
+    fallocate -l 2G /swapfile
+    chmod 0600 /swapfile
+    /sbin/mkswap /swapfile
+    /sbin/swapon /swapfile
 else
     echo "Did not confirm zero swap space, doing nothing."
 fi
