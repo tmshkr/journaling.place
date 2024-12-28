52261f9b57275dbfb4ed07b92b4c8026fb85f87a
Tim Shaker <tim@tmshkr.com>
2024-12-27 16:58:25 -0800

    update cdk stacks

--- packages/aws/cdk/index.ts
+++ packages/aws/cdk/index.ts
@@ -1,7 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { ALBStack } from "./stacks/alb";
 import { EC2Stack } from "./stacks/ec2";
-import { EfsStack } from "./stacks/efs";
 import { StorageStack } from "./stacks/storage";
 
 const env = {
@@ -11,5 +10,4 @@ const env = {
 const app = new cdk.App();
 new ALBStack(app, "ALBStack", { env });
 new EC2Stack(app, "EC2Stack", { env });
-new EfsStack(app, "EfsStack", { env });
 new StorageStack(app, "StorageStack", { env });
