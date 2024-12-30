27ee494403f58347e5ee84c94dd97143d9e59cb2
Tim Shaker <tim@tmshkr.com>
2024-12-30 10:44:41 -0800

    copy mailer package

--- scripts/copy-build-output.sh
+++ scripts/copy-build-output.sh
@@ -17,6 +17,7 @@ copy_build_output() {
     mkdir -p build_output/apps/agenda-worker/
     mkdir -p build_output/apps/trpc-server/
     mkdir -p build_output/apps/web/.next/
+    mkdir -p build_output/packages/mailer/
 
     print_heading "*" "Copying package.json"
 
@@ -30,6 +31,7 @@ copy_build_output() {
     cp -vR apps/agenda-worker/dist build_output/apps/agenda-worker/
     cp -vR apps/trpc-server/dist build_output/apps/trpc-server/
     cp -vR apps/web/.next build_output/apps/web/
+    cp -vR packages/mailer build_output/packages/mailer
 
     print_heading "*" "Copying node_modules"
 
