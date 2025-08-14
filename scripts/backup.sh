8de2a1254041c72aacefc4f3031d289d4bd9cbb3
tmshkr <tim@tmshkr.com>
2025-08-14 15:38:05 -0700

    add backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eox pipefail
+
+source .env
+
+required_env_vars=(
+    "BACKUP_BUCKET_NAME"
+    "MONGO_URI"
+    "TARGET_DOMAIN"
+)
+
+for env_var in "${required_env_vars[@]}"; do
+    if [[ -z "${!env_var}" ]]; then
+        echo "$env_var is required"
+        exit 1
+    fi
+done
+
+docker run --rm \
+    -v ./backup:/backup \
+    mongo:8.0.12 \
+    mongodump --uri "$MONGO_URI" --gzip --archive="/backup/backup.tar.gz"
+
+# Upload the backup to GCS
+gcloud storage cp backup.zip gs://$BACKUP_BUCKET_NAME/$TARGET_DOMAIN/backup.zip
