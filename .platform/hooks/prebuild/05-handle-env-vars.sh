ba80f718cddca354e982b4a987159c2b706fb69c
tmshkr <tim@tmshkr.com>
2025-08-23 16:52:31 -0700

    update env scripts

--- .platform/hooks/prebuild/05-handle-env-vars.sh
+++ .platform/hooks/prebuild/05-handle-env-vars.sh
@@ -7,26 +7,27 @@ get_config() {
 }
 
 environment_name=$(get_config container | jq -r '.environment_name')
-echo "EB_ENVIRONMENT_NAME=$environment_name" >>.env
+echo "EB_ENVIRONMENT_NAME=\"$environment_name\"" >.env
 
-get_config environment | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
+get_config environment | jq -r '. | to_entries[] | "\(.key)=\"\(.value)\""' | while read line; do
     echo $line >>.env
 done
 
-CNAME=$(aws elasticbeanstalk describe-environments --environment-names $environment_name --no-include-deleted | jq -r '.Environments[0].CNAME')
-echo "CNAME=$CNAME" >>.env
+eb_env=$(aws elasticbeanstalk describe-environments --environment-names $environment_name --no-include-deleted)
+CNAME=$(echo $eb_env | jq -r '.Environments[0].CNAME')
+VERSION_LABEL=$(echo $eb_env | jq -r '.Environments[0].VersionLabel')
+
+echo "CNAME=\"$CNAME\"" >>.env
+echo "VERSION_LABEL=\"$VERSION_LABEL\"" >>.env
+
 if [[ "$CNAME" == *production* ]]; then
-    echo "STAGE=production" >>.env
+    echo "STAGE=\"production\"" >>.env
 elif [[ "$CNAME" == *staging* ]]; then
-    echo "STAGE=staging" >>.env
+    echo "STAGE=\"staging\"" >>.env
 else
     echo "There was an error determining the environment stage"
     exit 1
 fi
-
-cat deploy-vars.json | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
-    echo $line >>.env
-done
 
 source .env
 # Retrieve parameters from AWS SSM
@@ -35,5 +36,5 @@ aws ssm get-parameters-by-path --path "/$repo/$STAGE/" \
     --recursive --with-decryption --query "Parameters[*].[Name,Value]" \
     --output text | while read -r name value; do
     key=$(echo $name | sed "s/\/$repo\/$STAGE\///")
-    echo "$key=$value" >>.env
+    echo "$key=\"$value\"" >>.env
 done
--- scripts/elasticbeanstalk/promote.sh
+++ scripts/elasticbeanstalk/promote.sh
@@ -1,21 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -eo xtrace
 
 get_config() {
     /opt/elasticbeanstalk/bin/get-config "$@"
 }
 
 environment_name=$(get_config container | jq -r '.environment_name')
-echo "EB_ENVIRONMENT_NAME=$environment_name" >.env
+echo "EB_ENVIRONMENT_NAME=\"$environment_name\"" >.env
 
-get_config environment | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
+get_config environment | jq -r '. | to_entries[] | "\(.key)=\"\(.value)\""' | while read line; do
     echo $line >>.env
 done
 
-echo "STAGE=production" >>.env
+eb_env=$(aws elasticbeanstalk describe-environments --environment-names $environment_name --no-include-deleted)
+CNAME=$(echo $eb_env | jq -r '.Environments[0].CNAME')
+VERSION_LABEL=$(echo $eb_env | jq -r '.Environments[0].VersionLabel')
 
-cat deploy-vars.json | jq -r '. | to_entries[] | "\(.key)=\(.value)"' | while read line; do
-    echo $line >>.env
-done
+echo "CNAME=\"$CNAME\"" >>.env
+echo "VERSION_LABEL=\"$VERSION_LABEL\"" >>.env
+echo "STAGE=\"production\"" >>.env
 
 source .env
 # Retrieve parameters from AWS SSM
@@ -24,7 +28,7 @@ aws ssm get-parameters-by-path --path "/$repo/$STAGE/" \
     --recursive --with-decryption --query "Parameters[*].[Name,Value]" \
     --output text | while read -r name value; do
     key=$(echo $name | sed "s/\/$repo\/$STAGE\///")
-    echo "$key=$value" >>.env
+    echo "$key=\"$value\"" >>.env
 done
 
 docker compose stop
