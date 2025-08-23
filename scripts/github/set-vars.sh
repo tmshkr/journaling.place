bb17e86a20f5c80b1b90b7e4f6d1e3121f766ee1
tmshkr <tim@tmshkr.com>
2025-08-22 18:42:57 -0700

    update stacks

--- scripts/github/set-vars.sh
+++ scripts/github/set-vars.sh
@@ -1,14 +1,16 @@
 #! /bin/bash -e
 
+key=$(openssl rand -hex 3)
+
 VpcStack=$(aws cloudformation describe-stacks --stack-name VpcStack)
 PublicSubnets=$(echo $VpcStack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="JpPublicSubnets") | .OutputValue')
 VpcId=$(echo $VpcStack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="JpVpcId") | .OutputValue')
 
-gh variable set BLUE_ENV --body "jp-blue"
-gh variable set GREEN_ENV --body "jp-green"
+gh variable set BLUE_ENV --body "jp-blue-$key"
+gh variable set GREEN_ENV --body "jp-green-$key"
 gh variable set PUBLIC_SUBNET_IDS --body $PublicSubnets
-gh variable set PRODUCTION_CNAME --body "jp-production"
-gh variable set STAGING_CNAME --body "jp-staging"
+gh variable set PRODUCTION_CNAME --body "jp-production-$key"
+gh variable set STAGING_CNAME --body "jp-staging-$key"
 gh variable set VPC_ID --body $VpcId
 
 echo "Updated GitHub Actions environment variables"
