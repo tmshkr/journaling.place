df06842b27ed69056741a08d85aa8b18ad48bbf2
tmshkr <tim@tmshkr.com>
2025-08-22 18:49:29 -0700

    add missing JpSecurityGroupId variable and ensure .env is sourced before key assignment

--- scripts/github/set-vars.sh
+++ scripts/github/set-vars.sh
@@ -1,15 +1,19 @@
 #! /bin/bash -e
 
-key=$(openssl rand -hex 3)
+source .env
+
+key=${VAR_KEY:-$(openssl rand -hex 3)}
 
 VpcStack=$(aws cloudformation describe-stacks --stack-name VpcStack)
 PublicSubnets=$(echo $VpcStack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="JpPublicSubnets") | .OutputValue')
 VpcId=$(echo $VpcStack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="JpVpcId") | .OutputValue')
+JpSecurityGroupId=$(echo $VpcStack | jq -r '.Stacks[0].Outputs[] | select(.ExportName=="JpSecurityGroupId") | .OutputValue')
 
 gh variable set BLUE_ENV --body "jp-blue-$key"
 gh variable set GREEN_ENV --body "jp-green-$key"
 gh variable set PUBLIC_SUBNET_IDS --body $PublicSubnets
 gh variable set PRODUCTION_CNAME --body "jp-production-$key"
+gh variable set SECURITY_GROUP_IDS --body $JpSecurityGroupId
 gh variable set STAGING_CNAME --body "jp-staging-$key"
 gh variable set VPC_ID --body $VpcId
 
