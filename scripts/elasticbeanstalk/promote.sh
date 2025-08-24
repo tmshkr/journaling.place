2f31ed3d88c693c85d430d749ca32fbdb169bfc2
tmshkr <tim@tmshkr.com>
2025-08-23 18:03:22 -0700

    remove service enable command in promote script

--- scripts/elasticbeanstalk/promote.sh
+++ scripts/elasticbeanstalk/promote.sh
@@ -36,7 +36,6 @@ docker compose up -d
 systemctl show -p PartOf eb-docker-compose-log.service
 systemctl daemon-reload
 systemctl reset-failed
-systemctl enable eb-docker-compose-log.service
 systemctl show -p PartOf eb-docker-compose-log.service
 systemctl is-active eb-docker-compose-log.service
 systemctl start eb-docker-compose-log.service
